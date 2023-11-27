da297bdcae1c2fca7346c63a76e6ac8af785789f
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2023-11-27 11:22:12 -0500

    Updated title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,9 @@ import { Metadata } from "next";
 import { cn } from "@/lib/utils";
 
 const title =
-  "Platforms Starter Kit – The all-in-one starter kit for building multi-tenant applications.";
+  "Gitwallet";
 const description =
-  "The Platforms Starter Kit is a full-stack Next.js app with multi-tenancy and custom domain support. Built with Next.js App Router, Vercel Postgres and the Vercel Domains API.";
+  "Gitwallet: Create & manage robust support tiers for your repos and ecosystems.";
 const image = "https://vercel.pub/thumbnail.png";
 
 export const metadata: Metadata = {
