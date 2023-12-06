38e7fbbf6c27bac4be8942c9d7afa58b7a05da52
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2023-12-06 07:14:26 -0400

    starting to build tiers page

--- app/app/(dashboard)/services/page.tsx
+++ app/app/(dashboard)/services/page.tsx
@@ -1,18 +1,39 @@
-export default function Tiers({ params }: { params: { id: string } }) {
+import tiers from './support-tiers';
+import TierVersions from './tier-versions';
+
+export default function Tiers() {
   return (
     <div className="flex max-w-screen-xl flex-col space-y-12 p-8">
       <div className="flex flex-col space-y-6">
-        <div className="flex items-center justify-between">
-          <h1 className="font-cal text-3xl font-bold dark:text-white">
-            Your Services
-          </h1>
-        </div>
-
-        <div className="flex flex-col space-y-2">
-          <h2>Manage your support tiers and services. We&apos;ve added a few to get you started.</h2>
-        </div>
-
+        <section className="bg-white dark:bg-gray-900">
+          <div className="mx-auto max-w-screen-xl lg:py-4">
+            <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
+              {tiers.map((tier, index) => (
+                <div key={index} className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
+                  <h3 className="mb-4 text-2xl font-semibold">{tier.name}</h3>
+                  <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">{tier.description}</p>
+                  <div className="flex justify-center items-baseline my-8">
+                    <span className="mr-2 text-5xl font-extrabold">{tier.price}</span>
+                    <span className="text-gray-500 dark:text-gray-400">{tier.frequency}</span>
+                  </div>
+                  <ul role="list" className="mb-8 space-y-0 text-left">
+                    {tier.features.map((feature, featureIndex) => (
+                      <li key={featureIndex} className="flex items-center space-x-3">
+                        <svg className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+                        <span>{feature}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <button type="button" className="px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get Started</button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
       </div>
+      <TierVersions></TierVersions>
     </div>
+
+
   );
 }
---
+++ app/app/(dashboard)/services/support-tiers.ts
@@ -0,0 +1,48 @@
+// supportTiers.ts
+const tiers = [
+    {
+      name: "Starter",
+      description: "Best option for personal use & for your next project.",
+      price: "$29",
+      frequency: "/month",
+      features: [
+        "Individual configuration",
+        "No setup, or hidden fees",
+        "Team size: 1 developer",
+        "Premium support: 6 months",
+        "Free updates: 6 months"
+      ],
+      link: "#"
+    },
+    {
+      name: "Company",
+      description: "Relevant for multiple users, extended & premium support.",
+      price: "$99",
+      frequency: "/month",
+      features: [
+        "Individual configuration",
+        "No setup, or hidden fees",
+        "Team size: 10 developers",
+        "Premium support: 24 months",
+        "Free updates: 24 months"
+      ],
+      link: "#"
+    },
+    {
+      name: "Enterprise",
+      description: "Best for large scale uses and extended redistribution rights.",
+      price: "$499",
+      frequency: "/month",
+      features: [
+        "Individual configuration",
+        "No setup, or hidden fees",
+        "Team size: 100+ developers",
+        "Premium support: 36 months",
+        "Free updates: 36 months"
+      ],
+      link: "#"
+    }
+  ];
+  
+  export default tiers;
+  
