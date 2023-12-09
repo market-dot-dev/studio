7885241eb4b3aaf3009cda38b8e74937d995b68a
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2023-12-09 08:25:00 -0400

    started building new tiers page

--- app/app/(dashboard)/services/tiers/new/page.tsx
+++ app/app/(dashboard)/services/tiers/new/page.tsx
@@ -1,16 +1,61 @@
+"use client";
+import { useState } from 'react';
 import PageHeading from '@/components/common/page-heading';
 
 export default function NewTier() {
+  const [tierName, setTierName] = useState('');
+  const [tierTagline, setTierTagline] = useState('');
+
   return (
-    <div className="flex max-w-screen-xl flex-col space-y-12 p-8">
+    <div className="flex max-w-screen-xl flex-col p-8">
       <PageHeading title="New Tier" />
 
-      <div className="flex flex-col space-y-6">
-        <section className="bg-white dark:bg-gray-900">
-          <div className="mx-auto max-w-screen-xl">
-                New Tier: Name, Description, Tagline, Services, Pricing
+      {/* Grid layout for responsiveness */}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        {/* Form Fields Section */}
+        <div className="md:col-span-2 space-y-6">
+          <div className="mb-4">
+            <label className="block mb-0.5 text-sm font-medium text-gray-900 dark:text-white">Tier Name</label>
+            <input
+              id="tierName"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeholder="Premium"
+              required
+              value={tierName}
+              onChange={(e) => setTierName(e.target.value)}
+            />
           </div>
-        </section>
+
+          <div className="mb-4">
+            <label className="block mb-0.5 text-sm font-medium text-gray-900 dark:text-white">Tier Tagline</label>
+            <input
+              id="tierTagline"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeholder="Great for startups and smaller companies."
+              required
+              value={tierTagline}
+              onChange={(e) => setTierTagline(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block mb-0.5 text-sm font-medium text-gray-900 dark:text-white">Tier Description</label>
+            <textarea
+              id="tierDescription"
+              rows={4}
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Describe your tier here. This is for your own use and will not be shown to any potential customers."
+            ></textarea>
+          </div>
+        </div>
+
+        {/* Preview Section */}
+        <div className="md:col-span-1 bg-white border border-gray-100 shadow p-4 rounded-lg">
+          <div>
+            <p className="text-sm text-gray-800">{tierName}</p>
+            <p className="text-sm text-gray-800">{tierTagline}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
