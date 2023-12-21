afad291a7bff3a28008b7267d91d525636d47e14
Aaron Graves <aaron.graves@gmail.com>
2023-12-21 14:50:16 -0500

    Local auth (dev-only)
    * local auth feature
      - only functions in dev environment
      - added a new env var DEV_OVERRIDE_PASSWORD
      - when set, you can visit /login/local-auth and log in as any gh user
    * cleaned up user/site creation logic (on first login)
    * added links to view pages on the parent domain instead of previewing on subdomain

---
+++ app/app/(auth)/login/dev-login-button.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import { toast } from "sonner";
+import LoadingDots from "@/components/icons/loading-dots";
+
+export default function DevLoginButton() {
+  const [loading, setLoading] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Get error message added by next/auth in URL.
+  const searchParams = useSearchParams();
+  const error = searchParams?.get("error");
+
+  useEffect(() => {
+    const errorMessage = Array.isArray(error) ? error.pop() : error;
+    errorMessage && toast.error(errorMessage);
+  }, [error]);
+
+  const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+
+    // Use signIn with the provided gh username and password
+    const res = await signIn("credentials", {
+      redirect: false,
+      gh_username: username,
+      password,
+    });
+
+    if (res?.error) {
+      toast.error(res.error);
+      setLoading(false);
+    } else {
+      // Redirect or handle successful login
+      window.location.href = '/';
+    }
+  };
+
+  return (
+    <form onSubmit={handleSignIn} className="space-y-4">
+      <div>
+        <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+          GitHub Username
+        </label>
+        <input
+          id="username"
+          name="username"
+          type="text"
+          required
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+          placeholder="Enter your GitHub username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+          Password
+        </label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          required
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+          placeholder="Enter the development backdoor password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button
+        type="submit"
+        disabled={loading}
+        className={`group my-2 flex w-full items-center justify-center rounded-md border border-stone-200 px-4 py-2 text-sm font-medium text-stone-600 transition-colors duration-75 focus:outline-none ${
+          loading
+            ? "cursor-not-allowed bg-stone-50 dark:bg-stone-800"
+            : "bg-white hover:bg-stone-50 active:bg-stone-100 dark:bg-black dark:hover:border-white dark:hover:bg-black"
+        }`}
+      >
+        {loading ? <LoadingDots color="#A8A29E" /> : "Sign in with Local Auth"}
+      </button>
+    </form>
+  );
+}
