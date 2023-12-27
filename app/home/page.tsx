22fdb132ec5ae69516b26a20a082e16c3bce06da
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2023-12-26 22:52:11 -0500

    first commit to work on new homepage

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,15 +4,8 @@ import Image from "next/image";
 export default function HomePage() {
   return (
     <div className="flex h-screen flex-col items-center justify-center space-y-10 bg-black">
-      <Image
-        width={512}
-        height={512}
-        src="/logo.png"
-        alt="Platforms on Vercel"
-        className="w-48"
-      />
       <h1 className="text-white">
-        Gitwallet (Alpha)
+        Gitwallet: the business builder for open source.
       </h1>
     </div>
   );
