828845d6c093af60717f4247a05bca5eab4b4ad3
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2024-01-22 21:53:41 -0500

    added page states for offerings page

--- components/internal-use/PageStates.ts
+++ components/internal-use/PageStates.ts
@@ -8,12 +8,7 @@ export enum CheckoutPageStates {
   PaymentFailed = "Payment Failed",
 }
 
-export enum ProjectPageStates {
-  Unregistered = "Logged Out",
-  Login = "Login Screen",
-  MagicLinkInput = "Magic Link Input",
-  RegisteredLoggedIn = "Registered, Logged In",
-  FirstPurchaseSuccess = "First Purchase Success",
-  nthPurchaseSuccess = "nth Purchase Success",
-  PaymentFailed = "Payment Failed",
+export enum OfferingPageStates {
+  ServiceOfferingUnselected = "All Offerings, None Selected",
+  ServiceOfferingSelected = "Service Offering Category Selected",
 }
