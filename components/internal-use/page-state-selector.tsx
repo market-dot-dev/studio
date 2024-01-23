a830baec52148bc08d3ed8aa544d86f687a76b5e
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2024-01-22 21:54:15 -0500

    add support for position in page state selector

--- components/internal-use/page-state-selector.tsx
+++ components/internal-use/page-state-selector.tsx
@@ -6,13 +6,14 @@ interface PageStateSelectorProps<T> {
     pageState: T;
     setPageState: React.Dispatch<React.SetStateAction<T>>;
     statesEnum: { [s: string]: T };
+    position: 'left' | 'right';
 }
 
-export default function PageStateSelector<T>({ pageState, setPageState, statesEnum }: PageStateSelectorProps<T>) {
+export default function PageStateSelector<T>({ pageState, setPageState, statesEnum, position }: PageStateSelectorProps<T>) {
 
     return (
-        <div className="fixed bottom-0 left-0 flex flex-row justify-center p-8 xl:px-32">
-            <Accordion className="my-2">
+        <div className={`fixed bottom-0 ${position === 'left' ? 'left-0' : 'right-0'} flex flex-row justify-center p-8`}>
+            <Accordion className="my-2 border-4 border-green-400">
                 <AccordionHeader className="my-0 py-1">Page States</AccordionHeader>
                 <AccordionBody>
                     <div className="flex flex-col gap-2 text-sm font-light leading-6 mb-4">
