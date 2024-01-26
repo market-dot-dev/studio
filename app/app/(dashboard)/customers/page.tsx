5cdf0d44f225fe43a8376d3cfdb1d0fe0e9085b4
gsgoraya <gsgoraya@gmail.com>
2024-01-26 17:38:16 +0530

    customers list for maintainer, in progress

--- app/app/(dashboard)/customers/page.tsx
+++ app/app/(dashboard)/customers/page.tsx
@@ -1,6 +1,12 @@
+import TierService from "@/app/services/TierService";
 import PageHeading from "@/components/common/page-heading";
+import { Divider, Flex } from "@tremor/react";
 
-export default function CustomersList({ params }: { params: { id: string } }) {
+export default async function CustomersList({ params }: { params: { id: string } }) {
+
+    const customers = await TierService.getCustomersOfUserTiers() as any[];
+    console.log(customers)
+
     return (
       <div className="flex max-w-screen-xl flex-col space-y-12 p-8">
         <div className="flex flex-col space-y-6">
@@ -9,6 +15,20 @@ export default function CustomersList({ params }: { params: { id: string } }) {
 
         <div className="flex flex-col space-y-2">
           <h2>Your active and past customers</h2>
+        </div>
+
+        <div className="flex flex-col space-y-2">
+          {customers.map((customer) => (
+            <>
+              <div key={customer.id} className="flex flex-col space-y-2">
+                <h2>{customer.name}</h2>
+                <p>{customer.email}</p>
+                <p>{customer.id}</p> 
+              </div>
+              <Divider />
+            </>
+          ))
+        }
         </div>
 
         </div>
