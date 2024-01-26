1f4240afdc0bed9f64590de216c35aa2773afb7e
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2024-01-26 08:50:56 -0500

    add save tier button to edit and new tier

--- app/app/(dashboard)/services/tiers/[id]/page.tsx
+++ app/app/(dashboard)/services/tiers/[id]/page.tsx
@@ -1,15 +1,27 @@
 import { findTier } from '@/app/services/TierService';
 import TierFormWrapper from './TierFormWrapper';
 import PageHeading from '@/components/common/page-heading';
+import { Button } from '@tremor/react';
 
-export default async function EditTierPage({params} : {params: { id: string }}) {
+export default async function EditTierPage({ params }: { params: { id: string } }) {
   const tier = await findTier(params.id);
-  if(!tier) return null;
-  
+  if (!tier) return null;
+
   return (
-    <div className="flex max-w-screen-xl flex-col p-8">
-    <PageHeading title="Edit Tier" />
+    <div className="flex max-w-screen-xl flex-col p-8 bg-white">
+      <div className="flex gap-4 mb-10 z-50">
+        <div className="flex justify-between w-full bg-white">
+          <PageHeading title="Edit Tier" />
+
+          <div className="flex flex-row">
+            <Button size='xs'>Save Tier</Button>
+          </div>
+        </div>
+        <div className="md:w-[300px] text-center" >
+        </div>
+      </div>
       <TierFormWrapper tier={tier} />
     </div>
+
   );
 }
--- app/app/(dashboard)/services/tiers/new/page.tsx
+++ app/app/(dashboard)/services/tiers/new/page.tsx
@@ -4,6 +4,7 @@ import PageHeading from '@/components/common/page-heading';
 import Tier, { newTier } from '@/app/models/Tier';
 import TierForm from '@/components/tiers/tier-form';
 import { useState } from 'react';
+import { Button } from '@tremor/react';
 
 const attrs = newTier();
 
@@ -15,9 +16,22 @@ export default function NewTierPage() {
   };
 
   return (
-    <div className="flex max-w-screen-xl flex-col p-8">
-      <PageHeading title="New Tier" />
+    <div className="flex max-w-screen-xl flex-col p-8 bg-white">
+      <div className="flex gap-4 mb-10 z-50">
+        <div className="flex justify-between w-full bg-white">
+          <PageHeading title="New Tier" />
+
+          <div className="flex flex-row">
+            <Button size='xs'>Save Tier</Button>
+          </div>
+        </div>
+        <div className="md:w-[300px] text-center" >
+        </div>
+      </div>
+
+
       <TierForm tier={tier} handleSubmit={handleSubmit} />
     </div>
+
   )
 }
