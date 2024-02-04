da8873b412f54ea4a6506d1f9bf7c38336725eb3
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2024-02-04 13:33:36 -0500

    cleaned up menu, still wip

--- components/nav.tsx
+++ components/nav.tsx
@@ -52,8 +52,8 @@ export default function Nav({ children, siteId, roleId }: { children: ReactNode,
           icon: <Settings width={18} />,
         },
       ];
-    } 
-    else if(urlSegments[0] === "page" && siteId) {
+    }
+    else if (urlSegments[0] === "page" && siteId) {
       return [
         {
           name: "Back to Site",
@@ -68,27 +68,6 @@ export default function Nav({ children, siteId, roleId }: { children: ReactNode,
         },
       ];
     }
-    else if (urlSegments[0] === "post" && id) {
-      return [
-        {
-          name: "Back to All Posts",
-          href: siteId ? `/site/${siteId}` : "/sites",
-          icon: <ArrowLeft width={18} />,
-        },
-        {
-          name: "Editor",
-          href: `/post/${id}`,
-          isActive: urlSegments.length === 2,
-          icon: <Edit3 width={18} />,
-        },
-        {
-          name: "Settings",
-          href: `/post/${id}/settings`,
-          isActive: urlSegments.includes("settings"),
-          icon: <Settings width={18} />,
-        },
-      ];
-    }
     return [
       {
         name: "Home",
@@ -97,21 +76,27 @@ export default function Nav({ children, siteId, roleId }: { children: ReactNode,
         icon: <LayoutDashboard width={18} />,
       },
       {
+        name: "Offerings",
+        href: "/offering",
+        isActive: urlSegments[0] === "offering",
+        icon: <Box width={18} />,
+      },
+      {
+        name: "Packages",
+        href: "/services/tiers",
+        isActive: urlSegments[0] === "services",
+        icon: <KanbanSquare width={18} />,
+      },
+      {
         name: "Customers",
         href: "/customers",
         isActive: urlSegments[0] === "customers",
         icon: <Users width={18} />,
       },
       {
-        name: "Services",
-        href: "/services/tiers",
-        isActive: urlSegments[0] === "services",
-        icon: <KanbanSquare width={18} />,
-      },
-      {
-        name: "Analytics",
-        href: "/analytics",
-        isActive: urlSegments[0] === "analytics",
+        name: "Reports",
+        href: "/reports",
+        isActive: urlSegments[0] === "reports",
         icon: <BarChart4 width={18} />,
       },
       {
@@ -125,13 +110,13 @@ export default function Nav({ children, siteId, roleId }: { children: ReactNode,
         href: "",
         isDivider: true,
       },
-      ...(siteId ? 
-      [{
-        name: "Your Site",
-        href: `/site/${siteId}`,
-        isActive: urlSegments[0] === "site",
-        icon: <Globe width={18} />,
-      }] : []),
+      ...(siteId ?
+        [{
+          name: "Your Site",
+          href: `/site/${siteId}`,
+          isActive: urlSegments[0] === "site",
+          icon: <Globe width={18} />,
+        }] : []),
       {
         name: "Embeds",
         href: "/channels/embeds",
@@ -145,11 +130,17 @@ export default function Nav({ children, siteId, roleId }: { children: ReactNode,
         icon: <Github width={18} />,
       },
       {
-        name: `Role: ${roleId}`,
+        name: "⚠️ DEBUG MENU ⚠️",
         href: "",
         isDivider: true,
       },
-      ...(['admin', 'maintainer'].includes(roleId || '') ? 
+      {
+        name: `Role: ${roleId}`,
+        href: "/",
+        isActive: urlSegments[0] === "/",
+        icon: <Cog width={18} />,
+      },
+      ...(['admin', 'maintainer'].includes(roleId || '') ?
         [{
           name: "Connect Stripe",
           href: `/maintainer/stripe-connect`,
@@ -175,33 +166,31 @@ export default function Nav({ children, siteId, roleId }: { children: ReactNode,
           urlSegments[0] === "post" && urlSegments.length === 2 && !showSidebar
             ? "left-5 top-5"
             : "right-5 top-7"
-        } sm:hidden`}
+          } sm:hidden`}
         onClick={() => setShowSidebar(!showSidebar)}
       >
         <Menu width={20} />
       </button>
       <div
-        className={`transform ${
-          showSidebar ? "w-full translate-x-0" : "-translate-x-full"
-        } fixed z-10 flex h-full flex-col justify-between border-r border-stone-200 bg-stone-100 p-4 transition-all dark:border-stone-700 dark:bg-stone-900 sm:w-60 sm:translate-x-0`}
+        className={`transform ${showSidebar ? "w-full translate-x-0" : "-translate-x-full"
+          } fixed z-10 flex h-full flex-col justify-between border-r border-stone-200 bg-stone-100 p-4 transition-all dark:border-stone-700 dark:bg-stone-900 sm:w-60 sm:translate-x-0`}
       >
         <div className="grid gap-2">
           <div className="flex items-center space-x-2 rounded-lg px-2 py-1.5">
-              <div className="text-md font-medium">
-                Gitwallet
-              </div>
+            <div className="text-md font-medium">
+              Gitwallet
+            </div>
           </div>
           <div className="grid gap-0.5">
             {tabs.map(({ name, href, isActive, icon }) => (
               href === "" ? (
-                  <span key={name} className="text-xs font-small uppercase mt-4">{name}</span>
-                ) : (
+                <span key={name} className="text-xs font-small uppercase mt-4">{name}</span>
+              ) : (
                 <Link
                   key={name}
                   href={href}
-                  className={`flex items-center space-x-3 ${
-                    isActive ? "bg-stone-200 text-black dark:bg-stone-700" : ""
-                  } rounded-lg px-1 py-0.5 transition-all duration-150 ease-in-out hover:bg-stone-200 active:bg-stone-300 dark:text-white dark:hover:bg-stone-700 dark:active:bg-stone-800`}
+                  className={`flex items-center space-x-3 ${isActive ? "bg-stone-200 text-black dark:bg-stone-700" : ""
+                    } rounded-lg px-1 py-0.5 transition-all duration-150 ease-in-out hover:bg-stone-200 active:bg-stone-300 dark:text-white dark:hover:bg-stone-700 dark:active:bg-stone-800`}
                 >
                   {icon}
                   <span className="text-sm font-medium">{name}</span>
@@ -212,7 +201,7 @@ export default function Nav({ children, siteId, roleId }: { children: ReactNode,
         </div>
         <div>
           <div className="grid gap-1">
-            
+
           </div>
           <div className="my-2 border-t border-stone-200 dark:border-stone-700" />
           {children}
