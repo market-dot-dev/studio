1f5e73e8b3a38c421f7412b2d367b808ba58d30a
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2024-02-06 10:16:31 -0500

    Update page.tsx

--- app/app/(dashboard)/settings/contract/page.tsx
+++ app/app/(dashboard)/settings/contract/page.tsx
@@ -3,7 +3,7 @@
 import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import UserService from "@/app/services/UserService";
-import { Title } from "@tremor/react";
+import { Bold, Button, Card, Select, Text } from "@tremor/react";
 
 export default async function ContractSettings() {
   const session = await getSession();
@@ -20,7 +20,27 @@ export default async function ContractSettings() {
 
   return (    
     <div className="space-y-6">
-      <Title>Contract Settings</Title>
+      <Bold>Choose A Contract:</Bold>
+
+      <Text>
+        Disclaimer: This is not legal advice. Please consult with a legal professional before using any of these contracts.
+      </Text>
+      <br />
+      <Bold>Gitwallet Standard MSA</Bold>
+      <Text>The Gitwallet Standard MSA is used for all agreements between maintainers and their customers. </Text>
+      <Button>Choose This Contract</Button>      <Button>View Contract</Button>
+
+      <br />      <br />
+      <Bold>Gitwallet Customized MSA</Bold>
+      <Text>The Gitwallet custom MSA is used for all agreements between maintainers and their customers. </Text>
+      <Select>
+        <option>Choose Cuntry</option>
+        <option>USA</option>
+        <option>Canada</option>
+        <option>UK</option>
+        <option>India</option>
+      </Select><Button>Choose This Contract</Button>      <Button>View Contract</Button>
+
     </div>
   );
 }
