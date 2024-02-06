92736118248df0af364b889aae86f5d1a0d6e127
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2024-02-06 08:45:38 -0500

    service categories wired up, wip

--- components/form/offering-form.tsx
+++ components/form/offering-form.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import { Select, Button, TextInput, SelectItem } from '@tremor/react';
+import { Bold, Text, Grid, Col, Card, Select, Button, TextInput, SelectItem } from '@tremor/react';
 import { TextArea } from "@radix-ui/themes"; // Corrected import for TextArea based on Radix UI
 import { Service, Feature } from '@prisma/client';
 import { update, create } from '@/app/services/feature-service';
@@ -61,6 +61,76 @@ const FeatureForm: React.FC<Props> = ({ service, initialFeature }) => {
   }, [initialFeature, setValue]);
 
   return (
+
+    <>
+
+                <Grid numItems={5} className="gap-2 mb-4">
+
+
+                    {/* 
+                    NOTHING SELECTED
+                    <Col numColSpan={2}>
+                        <div className="text-lg font-bold mb-2">Details</div>
+
+                        <div className="flex flex-col bg-gray-50 h-full pt-24 text-center h-full rounded-xl">
+                            Please select a fulfillment option to view details.
+                        </div>
+                    </Col>
+                    */}
+
+                    
+                    {/* COLUMN 1: SERVICE OFFERING CATEGORIES */}
+                    <Col numColSpan={1}>
+                        <div className="text-lg font-bold mb-2">Categories</div>
+
+                        <div className="flex flex-col pt-1">
+                            {service.map((s) => (
+                                // <Card className={feature.selected ? "border-2 border-slate-800 p-2 mb-2" : "p-2 mb-2"} key={s.index}>
+                                <Card className={s.selected ? "border-2 border-slate-800 p-2 mb-2" : "p-2 mb-2"} key={s.index}>
+                                    <div className="flex flex-row justify-items-center text-center gap-2">
+                                        {/* {feature.icon} */}
+                                        <Bold>{s.name}</Bold>
+                                    </div>
+                                </Card>
+                            ))}
+                        </div>
+                    </Col>
+
+{/* 
+                    <Col numColSpan={2}>
+                        <div className="text-lg font-bold mb-2">Options</div>
+                        <div className="flex flex-col h-4/5">
+                            {directContactDetails && (
+                                <div className="flex flex-col overflow-auto px-1 pt-1">
+                                    {directContactDetails.map((subcategory, subIndex) => (
+                                        <Card className={subcategory.checked ? "border-2 border-slate-800 p-2 mb-2" : "p-2 mb-2"} key={subIndex}>
+                                            <div className="flex flex-row items-center justify-between gap-2">
+                                                {subcategory.icon}
+                                                <Bold>{subcategory.title}</Bold>
+                                                <div className="ml-auto">
+                                                    <Switch defaultChecked={subcategory.checked} />
+                                                </div>
+                                            </div>
+                                            <div className="text-sm">{subcategory.description}</div>
+                                        </Card>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
+                    </Col>
+
+                    <Col numColSpan={2}>
+                        <div className="text-lg font-bold mb-2">Details</div>
+
+                        <div className="flex flex-col px-1">
+                            <label className="text-sm font-light">URL</label>
+                            <TextInput className="mb-4" placeholder="Enter your Discord server URL" />
+                            <label className="text-sm font-light">Details</label>
+                            <Textarea placeholder="Enter details about your Discord." />
+                        </div>
+                    </Col> */}
+                </Grid>
+    
     <form onSubmit={handleSubmit(onSubmit)}>
       {/* Manually manage Select state */}
       <Select 
@@ -89,6 +159,8 @@ const FeatureForm: React.FC<Props> = ({ service, initialFeature }) => {
       />
       <Button type="submit">Save</Button>
     </form>
+
+    </>
   );
 };
 
