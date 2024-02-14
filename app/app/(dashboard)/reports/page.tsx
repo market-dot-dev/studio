d00ad99b0687b65527fb5b86a37440ffab1001f1
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2024-02-13 23:21:23 -0500

    reports page

--- app/app/(dashboard)/reports/page.tsx
+++ app/app/(dashboard)/reports/page.tsx
@@ -1,25 +1,18 @@
-import OverviewStats from "@/components/overview-stats";
+import AllCharts from "@/components/overview-stats";
+import PageHeading from "@/components/common/page-heading";
+import { Text, Badge } from "@tremor/react";
 
-export default function AnalyticsPage({ params }: { params: { id: string } }) {
+export default function ReportsPage({ params }: { params: { id: string } }) {
   return (
     <div className="flex max-w-screen-xl flex-col space-y-12">
-      <div className="flex flex-col space-y-6">
-        <div className="flex items-center justify-between">
-          <h1 className="font-cal text-3xl font-bold dark:text-white">
-            Analytics
-          </h1>
+      <div className="flex justify-between w-full">
+        <div className="flex flex-col">
+          <PageHeading title="Reports" />
+          <Text>Your revenue & customer reports.</Text>
+          <Badge>Coming Soon</Badge>
         </div>
-
-        <div className="flex flex-col space-y-2">
-          <h2>Site and customer analytics</h2>
-        </div>
-
-        <div className="flex flex-col space-y-6">
-          <OverviewStats />
-        </div>
-
-
       </div>
+      <AllCharts />
     </div>
   );
 }
