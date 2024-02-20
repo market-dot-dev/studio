ae3b72f61077c401e67fbb344b079f93e4d2a8f8
Aaron Graves <aaron.graves@gmail.com>
2024-02-20 15:30:21 -0500

    properly scope features page to current user

--- app/app/(dashboard)/features/page.tsx
+++ app/app/(dashboard)/features/page.tsx
@@ -6,10 +6,11 @@ import React from 'react';
 import Offerings from './support-offerings';
 import { Text } from '@tremor/react';
 import PageHeading from '@/components/common/page-heading';
+import FeatureService from '@/app/services/feature-service';
 
 const OfferingsWrapper = async () => {
   const services: Service[] = await prisma.service.findMany();
-  const features: Feature[] = await prisma.feature.findMany();
+  const features: Feature[] = await FeatureService.findByCurrentUser();
 
   return (<>
       <div className="flex flex-col">
