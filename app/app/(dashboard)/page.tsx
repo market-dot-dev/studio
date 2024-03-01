7486008bbf8c1debdbd7e68fdefba54dec611454
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2024-02-29 21:16:58 -0500

    removed dupe onboarding guide (wtf?), and used getSessionUser

--- app/app/(dashboard)/page.tsx
+++ app/app/(dashboard)/page.tsx
@@ -1,16 +1,12 @@
 import DashboardCharts from "@/components/dashboard/dashboard-charts";
 import LatestCustomersList from "@/components/dashboard/latest-customer-list";
-import { getSession } from "@/lib/auth";
 import OnboardingGuide from "@/components/onboarding/onboarding-guide";
 import PageHeading from "@/components/common/page-heading";
 import SessionService from "@/app/services/SessionService";
-import StripeDisabledBanner from "@/components/common/stripe-disabled-banner";
 
 export default async function Overview() {
-  const session = await getSession();
-  const title = session?.user.name ? `Welcome, ${session.user.name}!` : "Your Dashboard";
-
   const user = await SessionService.getSessionUser();
+  const title = user?.name ? `Welcome, ${user.name}!` : "Your Dashboard";
 
   return (
     <>
@@ -18,15 +14,9 @@ export default async function Overview() {
         <div className="text-center">
           <OnboardingGuide dashboard={true} />
         </div>
-        <div className="text-center">
-          <OnboardingGuide dashboard={true} />
-        </div>
         <div className="flex flex-col space-y-6">
           <PageHeading title={title} />
           <div className="flex flex-col gap-8">
-            { /* FIXME: reenable once banner is styled -- also need to health check on login or somesuch
-              user?.stripeAccountDisabled && <StripeDisabledBanner /> */}
-
             <LatestCustomersList numRecords={3} previewMode={true} />
             <DashboardCharts />
           </div>
