f6a0319d4d5c7d75529379fff822dd94114bee1a
Aaron Graves <aaron.graves@gmail.com>
2024-03-21 14:57:36 -0400

    fix migrations

--- prisma/migrations/20240321002026_add_customer_id_hash_to_users/migration.sql
+++ prisma/migrations/20240321002026_add_customer_id_hash_to_users/migration.sql
@@ -5,6 +5,35 @@
 
 */
 -- AlterTable
+
 ALTER TABLE "User"
 ADD COLUMN "stripeCustomerIds" JSONB NOT NULL DEFAULT '{}',
 ADD COLUMN "stripePaymentMethodIds" JSONB NOT NULL DEFAULT '{}';
+  
+UPDATE "User"
+SET "stripeCustomerIds" = jsonb_set(
+    -- coalesce is not strictly necessary since the column now has a NOT NULL DEFAULT constraint
+    "stripeCustomerIds",
+    '{gitwallet}', 
+    -- Handle the possibility of null values by storing them as an empty JSON object
+    CASE 
+        WHEN "stripeCustomerId" IS NOT NULL THEN to_jsonb("stripeCustomerId")
+        ELSE '{}'::jsonb
+    END
+),
+"stripePaymentMethodIds" = jsonb_set(
+    "stripePaymentMethodIds",
+    '{gitwallet}', 
+    CASE 
+        WHEN "stripePaymentMethodId" IS NOT NULL THEN to_jsonb("stripePaymentMethodId")
+        ELSE '{}'::jsonb
+    END
+);
+
+-- DropIndex
+DROP INDEX "User_stripeCustomerId_key";
+DROP INDEX "User_stripePaymentMethod_key";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "stripeCustomerId";
+ALTER TABLE "User" DROP COLUMN "stripePaymentMethodId";
