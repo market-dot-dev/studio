d0542120bcd3ef4e996bb69dde5faa53799b5c8b
gsgoraya <gsgoraya@gmail.com>
2024-03-26 23:17:21 +0530

    site project description to display line breaks

--- components/site/insertables/site-description/site-description.tsx
+++ components/site/insertables/site-description/site-description.tsx
@@ -1,6 +1,10 @@
-'use client'
 export default function SiteDescription({site, page}: { site : any, page : any}) {
+    const lines = site.user.projectDescription.split('\n');
+    const html = lines.join('<br />');
+    // console.log('lines', lines)
+    // const paragraphs = lines.map((line : string, index : number) => <p key={index}>{line}</p>);
     return (
-        <>{site.user.projectDescription}</>
+        <span dangerouslySetInnerHTML={{__html: html}} />
     )
+    
 }
