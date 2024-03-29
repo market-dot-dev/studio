65e00d9d4834bd470d1807d7f9181582e379795e
Aaron Graves <aaron.graves@gmail.com>
2024-03-29 16:37:06 -0400

    vercel guard

--- scripts/dump-load.sh
+++ scripts/dump-load.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ "$VERCEL" == "1" ]; then
+  echo "This script is intended for local development only. Exiting..."
+  exit 0
+fi
+
 dropdb gitwallet_prod
 createdb gitwallet_prod
 pg_restore -d gitwallet_prod ../gitwallet_prod.dump
