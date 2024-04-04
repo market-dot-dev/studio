78e1dd66b44442dba7ef81779b19e174eb86022d
gsgoraya <gsgoraya@gmail.com>
2024-04-04 09:09:36 +0530

    reporting for desktop widgets in progress

---
+++ app/services/ReportingService.tsx
@@ -0,0 +1,30 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+class ReportingService {
+    static async getSubscriptionsCreated() {
+
+        return await prisma.subscription.findMany({
+            where: {
+              createdAt: {
+                gte: new Date(new Date().getFullYear(), 0, 1), // Start of the current year
+                lte: new Date(new Date().getFullYear(), 11, 31) // End of the current year
+              }
+            },
+            select: {
+                createdAt: true,
+                tier: {
+                  select: {
+                    price: true 
+                  }
+                }
+            }
+            
+
+        });
+    }
+}
+
+export default ReportingService;
+export const { getSubscriptionsCreated } = ReportingService;
