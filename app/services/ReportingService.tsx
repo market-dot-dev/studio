664a87ac21857a9d8e17b3b08d0d9b28e1756a82
gsgoraya <gsgoraya@gmail.com>
2024-04-08 13:18:26 +0530

    homepage charts fixed

--- app/services/ReportingService.tsx
+++ app/services/ReportingService.tsx
@@ -1,16 +1,22 @@
 "use server";
 
 import prisma from "@/lib/prisma";
+import SessionService from "./SessionService";
 
 class ReportingService {
     static async getSubscriptionsCreated() {
-
+        
+      const userId = await SessionService.getCurrentUserId();
+        
         return await prisma.subscription.findMany({
             where: {
               createdAt: {
                 gte: new Date(new Date().getFullYear(), 0, 1), // Start of the current year
                 lte: new Date(new Date().getFullYear(), 11, 31) // End of the current year
-              }
+              },
+              tier: {
+                userId: userId, // owner of the tier
+              },
             },
             select: {
                 createdAt: true,
@@ -20,8 +26,6 @@ class ReportingService {
                   }
                 }
             }
-            
-
         });
     }
 }
