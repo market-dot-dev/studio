e21b114326ca3cacbe0722721c1c5702d954f02b
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2024-04-16 22:46:17 -0400

    updates to tier card with switch

--- components/tiers/tier-card.tsx
+++ components/tiers/tier-card.tsx
@@ -24,10 +24,18 @@ const GetStartedButton = ({ url, tierId, canEdit, annual = false }: { url: strin
   return (
     <Link href={linkUrl}>
       <Button variant={variant} className="w-full">
-        {annual ? `Purchase Annual` : `Get Started`}
+        {annual ? `Get Started` : `Get Started`}
       </Button>
     </Link>
   );
+}
+
+
+const calcDiscount = (price: number, annualPrice: number) => {
+	if (price === 0) return 0;
+	if (annualPrice === 0) return 100;
+	const twelveMonths = price * 12;
+	return Math.round(((twelveMonths - annualPrice) / twelveMonths * 100) * 10) / 10;
 }
 
 
@@ -39,31 +47,42 @@ const TierCard: React.FC<TierCardProps> = ({ tier, url = null, canEdit = false, 
   const hasAnnual = (tier.priceAnnual || 0) > 0;
   const isntOnce = tier.cadence !== 'once'
   const [showAnnual, setShowAnnual] = useState(false);
-
   const textClasses = darkMode ? "text-gray-400" : "text-gray-500";
   // check if this componenet has been called with features passed to it (e.g. when editing tier in preview)
   const directlyProvidedFeatures = !!features && features.length > 0;
+	const [annualDiscountPercent, setAnnualDiscountPercent] = useState(calcDiscount(tier.price, tier.priceAnnual || 0));
 
   // if features are directly provided, use them, otherwise use the features from the tier
   const tierFeatures = (directlyProvidedFeatures ? features : tier.features) || [];
 
+
+
   return (<>
     <Card className={`flex flex-col p-6 mx-auto w-full h-full justify-between max-w-xs text-center rounded-lg border shadow ${containerClasses}`}>
       <div>
-        <h3 className={`mb-2 text-2xl font-bold ${textClasses}`}>{tier.name}</h3>
-        <p className="font-light text-gray-500">{tier.tagline}</p>
-        <div>
+        <h3 className={`mb-4 text-2xl font-bold ${textClasses}`}>{tier.name}</h3>
+        <p className="mb-4 font-light text-sm text-gray-500 mb-2">{tier.tagline}</p>
+        <div className="text-center">
           {hasAnnual && isntOnce ? <>
-            Annual
-            <Switch checked={showAnnual} onChange={() => setShowAnnual(!showAnnual)} /> 
+            <div className="justify-center flex gap-1">
+              <Text>Monthly</Text>
+              <Switch checked={showAnnual} onChange={() => setShowAnnual(!showAnnual)} /> 
+              <Text>Annual</Text>
+            </div>
           </>: null}
         </div>
 
         <div className="flex justify-center items-baseline my-4">
-          <span className={`mr-1 text-4xl font-extrabold ${textClasses}`}>${showAnnual ? tier.priceAnnual : tier.price}</span>
+          <span className={`mr-1 text-4xl font-extrabold ${textClasses}`}>${showAnnual ? Math.round((tier.priceAnnual || 0) / 12) : tier.price}</span>
           /&nbsp;
-          <span className="text-gray-500 dark:text-gray-400">{showAnnual ? 'per year' : tier.cadence}</span>
+          {/* <span className="text-gray-500 dark:text-gray-400">{showAnnual ? 'per year' : tier.cadence}</span> */}
+          <span className="text-gray-500 dark:text-gray-400">{tier.cadence}</span>
         </div>
+
+        <div className="flex justify-center items-baseline my-4">
+          <span className={`mr-1 text-4xl font-extrabold ${textClasses}`}>{}</span>
+        </div>
+
         <Text className="text-center text-xs text-gray-400">What&apos;s Included:</Text>
         <TierFeatureList features={tierFeatures}  darkMode={darkMode} />
       </div>
