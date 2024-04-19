f5d06e18b533d900ea34616429ed3c8f28d2d60c
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2024-04-18 22:37:44 -0400

    wip - update lead item

--- components/leads/lead-item.tsx
+++ components/leads/lead-item.tsx
@@ -1,9 +1,11 @@
 import { Lead } from "@prisma/client";
 import { Badge, Text, Bold, Button } from "@tremor/react";
 import { useState } from "react";
+import { ProgressCircle } from '@tremor/react';
 
-function getDependencyBadge(dependentReposCount: number) {
-    return dependentReposCount > 5 ? "High Dependency" : "Low Dependency";
+
+function getDependencyScoreBadge(dependentReposCount: number) {
+    return dependentReposCount > 5 ? "High" : "Low";
 }
 
 function getWebsiteBadge(website: string) {
@@ -15,12 +17,14 @@ function getWebsiteBadge(website: string) {
     try {
         const domain = new URL(website).hostname.split('.').pop();
         switch (domain) {
+            case 'com': 
+                return 'Company';
             case 'edu':
                 return 'Educational';
             case 'org':
                 return 'Non-profit';
             default:
-                return 'Commercial';
+                return;
         }
     } catch (error) {
         console.error("Error parsing URL:", error);
@@ -37,7 +41,7 @@ export default function LeadItem({ lead }: { lead: Lead }) {
     };
 
     const visibleMaintainers = showAllMaintainers ? lead.maintainers as [] : (lead.maintainers as []).slice(0, 10);
-
+    const dependencyPercentage = Math.round((lead.dependent_repos_count / lead.repositories_count) * 100);
     return (
         <>
             <div className="flex flex-row gap-8 mb-2">
@@ -48,14 +52,26 @@ export default function LeadItem({ lead }: { lead: Lead }) {
                 <div className="flex flex-col">
                     <div className="flex gap-4">
                         <Badge>{lead.kind && lead.kind.charAt(0).toUpperCase() + lead.kind.slice(1)}</Badge>
-                        <Badge>{getDependencyBadge(lead.dependent_repos_count)}</Badge>
-                        { lead.website ? <Badge>{getWebsiteBadge(lead.website)}</Badge> : null }
                     </div>
                     <Bold>{lead.name}</Bold>
                     <Text>{lead.description}</Text>
                     <Text><a href={lead.html_url} target="_blank" className="underline">{lead.html_url}</a></Text>
                 </div>
             </div>
+            <div className="flex justify-start space-x-5 items-center">
+            <ProgressCircle value={dependencyPercentage} size="md">
+              <span className="text-xs font-medium text-slate-700">{dependencyPercentage}%</span>
+            </ProgressCircle>
+            <div>
+              <p className="text-tremor-default text-tremor-content-strong dark:text-dark-tremor-content-strong font-medium">
+                Used by {lead.dependent_repos_count} of {lead.repositories_count} repositories.
+              </p>
+              <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+                Dependency Score:
+                <Badge>{<>getDependencyScoreBadge</>}</Badge>
+              </p>
+            </div>
+          </div>
 
             <Text>Dependent Repositories: {lead.dependent_repos_count}</Text>
             <Text>Total Repositories: {lead.repositories_count}</Text>
