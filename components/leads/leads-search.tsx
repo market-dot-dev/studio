bd6fab1cb200ee5493e0ad91a331d123b88fc548
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2024-04-21 01:14:52 -0400

    Update leads-search.tsx

--- components/leads/leads-search.tsx
+++ components/leads/leads-search.tsx
@@ -1,5 +1,5 @@
 'use client'
-import {  Lead, Repo } from "@prisma/client";
+import { Lead, Repo } from "@prisma/client";
 import { Bold, Card, Badge, SearchSelect, SearchSelectItem, Button, Text, NumberInput, Switch, SelectItem, Select, TextInput } from "@tremor/react";
 import Link from "next/link";
 import { Search } from "lucide-react";
@@ -23,12 +23,12 @@ type LeadKey = {
 }
 
 export default function LeadsSearch({ repos }: { repos: Repo[] }) {
-    
+
     const [selectedRepo, setSelectedRepo] = useState<Repo | null>(null);
     const [radarId, setRadarId] = useState<number | null>(null);
     const [shortListedLeads, setShortListedLeads] = useState<LeadKey[]>([]);
     const [radarResults, setRadarResults] = useState<any[]>([]);
-    
+
     const [isSearching, setIsSearching] = useState<boolean>(false);
     const [page, setPage] = useState<number>(1);
     const [perPage, setPerPage] = useState<number>(20);
@@ -58,28 +58,28 @@ export default function LeadsSearch({ repos }: { repos: Repo[] }) {
     }, [])
 
     const lookupRepo = useCallback((initial?: boolean) => {
-        if( selectedRepo?.url ) {
+        if (selectedRepo?.url) {
             try {
                 lookup(selectedRepo.url).then((res: any) => {
 
-                    if( res.error ) {
-                        if( initial ) {
+                    if (res.error) {
+                        if (initial) {
                             setSearchError(res.error);
                             setIsSearching(false);
                         } else {
-                            console.log( errorMessages.FAILED_GET_OWNERS_COUNT, res.error);
+                            console.log(errorMessages.FAILED_GET_OWNERS_COUNT, res.error);
                         }
 
                         return;
                     }
-                    
+
                     const newTotalCount = res.data.dependent_owners_count ?? 0;
 
                     setTotalCount(newTotalCount);
 
-                    if( initial ) {
-                        if(!newTotalCount) {
-                            setSearchError( errorMessages.NO_DEPENDENT_OWNERS );
+                    if (initial) {
+                        if (!newTotalCount) {
+                            setSearchError(errorMessages.NO_DEPENDENT_OWNERS);
                             setIsSearching(false);
                         }
                         // Reset page to 1 as it is the initial lookup for a new repo
@@ -90,21 +90,21 @@ export default function LeadsSearch({ repos }: { repos: Repo[] }) {
 
                     } else {
                         // if the count has changed
-                        if( totalCount !== newTotalCount ) {
+                        if (totalCount !== newTotalCount) {
                             clearLeadsCache();
                         }
 
                     }
 
-                    
+
                     setOrgsCount(res.data.dependent_organizations_count ?? 0);
                 });
             } catch (error) {
-                console.log( errorMessages.FAILED_GET_OWNERS_COUNT, error)
+                console.log(errorMessages.FAILED_GET_OWNERS_COUNT, error)
                 setIsSearching(false);
-            } 
+            }
         }
-    }, [ selectedRepo, setTotalCount, setOrgsCount, lookup, totalCount, setSearchError, setIsSearching ])
+    }, [selectedRepo, setTotalCount, setOrgsCount, lookup, totalCount, setSearchError, setIsSearching])
 
     const getDependentOwnersWithCache = useCallback(async () => {
 
@@ -126,8 +126,8 @@ export default function LeadsSearch({ repos }: { repos: Repo[] }) {
         }
         try {
             const res = await getDependentOwners(radarId!, page, perPage, showOnlyOrgs);
-            
-            if( res.error ) {
+
+            if (res.error) {
                 setSearchError(res.error);
                 return [];
             }
@@ -140,15 +140,15 @@ export default function LeadsSearch({ repos }: { repos: Repo[] }) {
             return res.data;
 
         } catch (error) {
-            
+
             setSearchError(errorMessages.FAILED_GET_OWNERS);
             return [];
         }
     }, [radarId, page, perPage, showOnlyOrgs, setSearchError]);
 
     useEffect(() => {
-        
-        if( !selectedRepo?.url ) {
+
+        if (!selectedRepo?.url) {
             return;
         }
 
@@ -163,66 +163,74 @@ export default function LeadsSearch({ repos }: { repos: Repo[] }) {
     }, [selectedRepo])
 
     useEffect(() => {
-        if( !radarId ) {
+        if (!radarId) {
             return;
         }
 
-        if(!totalCount) {
+        if (!totalCount) {
             setSearchError(errorMessages.HAS_NO_OWNERS);
             setRadarResults([]);
             setIsSearching(false);
             return;
         }
-        
+
         setSearchError('');
         setIsSearching(true);
         lookupRepo();
-        
+
         getDependentOwnersWithCache().then((fetchedLeads: any[]) => {
             setRadarResults(fetchedLeads);
         })
-        .finally(() => {
-            setIsSearching(false);
-        });
+            .finally(() => {
+                setIsSearching(false);
+            });
 
     }, [page, perPage, showOnlyOrgs, radarId]);
 
     const resultsPerPage = (
         <>
-        <div className="text-sm">Results per Page</div>
-        <div>
-            <Select 
-                disabled={isSearching}
-                className="w-24" value={`${perPage}`} onValueChange={(e: string) => {
-                setPerPage(parseInt(e));
-            }}>
-                <SelectItem value="20">20</SelectItem>
-                <SelectItem value="50">50</SelectItem>
-                <SelectItem value="100">100</SelectItem>
-            </Select>
-        </div>
+            <div className="text-sm">Results per Page</div>
+            <div>
+                <Select
+                    disabled={isSearching}
+                    className="w-24" value={`${perPage}`} onValueChange={(e: string) => {
+                        setPerPage(parseInt(e));
+                    }}>
+                    <SelectItem value="20">20</SelectItem>
+                    <SelectItem value="50">50</SelectItem>
+                    <SelectItem value="100">100</SelectItem>
+                </Select>
+            </div>
         </>
     )
 
     return (
         <>
             <div className="mb-4">
-                <Bold>Search for a Repo:</Bold>
 
                 <div className="flex flex-row gap-4">
-                <TextInput icon={Search} placeholder="Enter a Repo URL..." />
-                
-
+                    <TextInput icon={Search} placeholder="Enter a Repo URL..." />
                 </div>
+                {/* <Text>You can only search for connected repositories. To connect more repositories, go to your <Link href="/settings/repos" className="underline">Repository Settings</Link>.</Text> */}
 
-                <Text>You can only search for connected repositories. To connect more repositories, go to your <Link href="/settings/repos" className="underline">Repository Settings</Link>.</Text>
-                { repos.length ?
-                    <SearchSelect onValueChange={handleRepoSelected} className="relative z-[100]" >
-                        {repos.map((repo, index) => (
-                            <SearchSelectItem key={index} value={`${index}`}>{repo.name}</SearchSelectItem>
-                        ))}
-                    </SearchSelect>
-                :
+                {repos.length ?
+                    <>
+                        {/* <SearchSelect onValueChange={handleRepoSelected} className="relative z-[100]" >
+                            {repos.map((repo, index) => (
+                                <SearchSelectItem key={index} value={`${index}`}>{repo.name}</SearchSelectItem>
+                            ))}
+                        </SearchSelect> */}
+                        <div className="flex-col gap-2 mt-2 md:flex-row">
+                            <Text>Search for your connected repos: (<Link href="/settings/repos" className="underline">Connect More</Link>)</Text>
+                            <div className="flex gap-2 mt-2">
+                                {repos.map((repo, index) => (
+                                    // <SearchSelectItem key={index} value={`${index}`}>{repo.name}</SearchSelectItem>
+                                    <Button size="xs" className="rounded-xl py-0 px-2">{repo.name}</Button>
+                                ))}
+                            </div>
+                        </div>
+                    </>
+                    :
                     <Card
                         className="my-4 w-full bg-gray-100 border border-gray-400 px-4 py-3 text-gray-700"
                     >
@@ -234,10 +242,10 @@ export default function LeadsSearch({ repos }: { repos: Repo[] }) {
                 }
 
             </div>
-            
-            <Bold>Search Results{ selectedRepo?.name ? ' for ' + selectedRepo.name : ''}:</Bold>
-            { searchError ? <Text className="text-red-500">{searchError}</Text> : null }
-            { radarResults.length ?
+
+            <Bold>Search Results{selectedRepo?.name ? ' for ' + selectedRepo.name : ''}:</Bold>
+            {searchError ? <Text className="text-red-500">{searchError}</Text> : null}
+            {radarResults.length ?
                 <div className="flex flex-col gap-4 items-stretch sticky -top-1 z-10 bg-white p-4 shadow-sm border rounded-md -mr-1 -ml-1">
                     <div className="flex justify-between items-center w-full">
 
@@ -248,12 +256,12 @@ export default function LeadsSearch({ repos }: { repos: Repo[] }) {
                             onPageChange={handlePageChange}
                             isLoading={isSearching}
                         />
-                        
-                        
+
+
                         <div className="hidden items-center justify-end gap-3 w-1/3 lg:flex">
                             {resultsPerPage}
                         </div>
-                        
+
                     </div>
                     <div className="flex justify-between items-center w-full border border-t-1 border-b-0 border-l-0 border-r-0 pt-4">
                         <div className="flex items-center justify-center gap-3">
@@ -274,39 +282,39 @@ export default function LeadsSearch({ repos }: { repos: Repo[] }) {
                         </div>
                     </div>
                 </div>
-                :null
-                }
-                <div className="w-full min-h-[100vh]">
-                    { isSearching ? 
+                : null
+            }
+            <div className="w-full min-h-[100vh]">
+                {isSearching ?
                     <div className="flex justify-center items-center sticky top-[200px] z-50">
                         <LoadingSpinner />
-                    </div> 
-                    : 
+                    </div>
+                    :
                     <>
-                        { radarResults?.map((lead, index) => {
+                        {radarResults?.map((lead, index) => {
                             const isShortlisted = shortListedLeads.some(leadKey => leadKey.host === lead.host && leadKey.uuid === lead.uuid);
-                            return <SearchResult key={index} lead={lead} isShortlisted={isShortlisted} setShortListedLeads={setShortListedLeads} selectedRepo={selectedRepo} />   
+                            return <SearchResult key={index} lead={lead} isShortlisted={isShortlisted} setShortListedLeads={setShortListedLeads} selectedRepo={selectedRepo} />
                         })}
-                        
-                    </>
-                
-                    }
-                </div>
 
-            
-                
+                    </>
+
+                }
+            </div>
+
+
+
         </>
     )
 }
 
 
 
-function SearchResult( { selectedRepo, lead, isShortlisted, setShortListedLeads } : { selectedRepo: Repo | null, lead: Lead, isShortlisted: boolean, setShortListedLeads: any}) {
+function SearchResult({ selectedRepo, lead, isShortlisted, setShortListedLeads }: { selectedRepo: Repo | null, lead: Lead, isShortlisted: boolean, setShortListedLeads: any }) {
     const [isAddingToShortlist, setIsAddingToShortlist] = useState<boolean>(false);
 
-    const addToShortlist = useCallback((lead : any) => {
-        
-        if( !selectedRepo ) {
+    const addToShortlist = useCallback((lead: any) => {
+
+        if (!selectedRepo) {
             return;
         }
 
@@ -315,12 +323,12 @@ function SearchResult( { selectedRepo, lead, isShortlisted, setShortListedLeads 
         addLeadToShortlist(lead, selectedRepo.id).then(res => {
             setShortListedLeads((prev: LeadKey[]) => [...prev, { host: lead.host, uuid: lead.uuid }]);
         })
-        .catch((err: any) => {
-            console.error('Failed to add to shortlist:', err);
-        })
-        .finally(() => {
-            setIsAddingToShortlist(false);
-        });
+            .catch((err: any) => {
+                console.error('Failed to add to shortlist:', err);
+            })
+            .finally(() => {
+                setIsAddingToShortlist(false);
+            });
     }, [selectedRepo])
     return (
         <Card className="flex flex-col my-2 z-0 relative">
@@ -330,7 +338,7 @@ function SearchResult( { selectedRepo, lead, isShortlisted, setShortListedLeads 
                 <Button
                     loading={isAddingToShortlist}
                     disabled={isShortlisted || isAddingToShortlist}
-                    onClick={() => addToShortlist(lead)} 
+                    onClick={() => addToShortlist(lead)}
                 >Add to Shortlist</Button>
             </div>
         </Card>
@@ -350,7 +358,7 @@ function Pagination({ page, perPage, totalCount, onPageChange, isLoading }: { pa
 
     const handleGoClick = useCallback(() => {
         if (!isNaN(inputPage) && inputPage !== page) {
-            changePage(inputPage); 
+            changePage(inputPage);
         }
     }, [page, inputPage]);
 
@@ -359,7 +367,7 @@ function Pagination({ page, perPage, totalCount, onPageChange, isLoading }: { pa
     }, [page])
 
     return (
-        
+
         <div className="flex justify-start items-center gap-8 w-full lg:w-2/3">
             <div className="flex justify-center items-center gap-2">
                 <Button
@@ -370,7 +378,7 @@ function Pagination({ page, perPage, totalCount, onPageChange, isLoading }: { pa
                     Prev
                 </Button>
                 <span className="text-sm font-medium">Page {page} of {totalPages}</span>
-                
+
                 <Button
                     size="xs"
                     onClick={() => changePage(page + 1)}
@@ -380,7 +388,7 @@ function Pagination({ page, perPage, totalCount, onPageChange, isLoading }: { pa
                 </Button>
             </div>
             <div className="flex justify-end lg:justify-center items-center gap-2 grow">
-                
+
                 <input
                     type="number"
                     // enableStepper={false}
@@ -393,7 +401,7 @@ function Pagination({ page, perPage, totalCount, onPageChange, isLoading }: { pa
                     className="border rounded-md text-xs w-20 border-gray-300"
                     placeholder="Go to page..."
                 />
-                
+
                 <Button
                     size="xs"
                     disabled={isLoading}
@@ -402,7 +410,7 @@ function Pagination({ page, perPage, totalCount, onPageChange, isLoading }: { pa
                 </Button>
             </div>
         </div>
-            
-        
+
+
     );
 }
