3d6f75fce88e73325e1af9e6e1e6ae780fa25520
Aaron Graves <aaron.graves@gmail.com>
2024-05-22 15:19:02 -0400

    restore formatting

--- app/app/(dashboard)/customers/page.tsx
+++ app/app/(dashboard)/customers/page.tsx
@@ -3,12 +3,22 @@ import { useState, useEffect } from 'react';
 import { User, Subscription, Charge } from '@prisma/client';
 import { customersOfMaintainer } from '@/app/services/UserService';
 import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeaderCell,
+  TableRow,
   Text,
 } from '@tremor/react';
 import React from 'react';
 import Tier from '@/app/models/Tier';
 import useCurrentSession from '@/app/hooks/use-current-session';
 import PageHeading from '@/components/common/page-heading';
+import LinkButton from '@/components/common/link-button';
+import DashboardCard from '@/components/common/dashboard-card';
+import SubscriptionStatusBadge from './subscription-state';
+import PurchaseStatusBadge from './purchase-state';
 
 const formatDate = (date: Date | string): string => {
   const options: Intl.DateTimeFormatOptions = {
@@ -39,28 +49,19 @@ type SubscriptionRowProps = {
 };
 
 const SubscriptionRow = ({ user, subscription }: SubscriptionRowProps) => {
-  const handleCancelSubscription = async () => {
-    // Implement the logic to cancel the subscription
-    console.log('Canceling subscription:', subscription.id);
-  };
-
-  const handleUpdateSubscription = async () => {
-    // Implement the logic to update the subscription
-    console.log('Updating subscription:', subscription.id);
-  };
-
   return (
-    <tr>
-      <td>{user.name}</td>
-      <td>{user.company || '(unknown)'}</td>
-      <td><a href={`mailto:${user.email}`}>{user.email}</a></td>
-      <td>{subscription.tier.name}</td>
-      <td>{subscription.state}</td>
-      <td>{formatDate(subscription.createdAt)}</td>
-      <td>
-        <button onClick={() => { window.location.href = `/customers/${user.id}` }}>View</button>
-      </td>
-    </tr>
+    <TableRow className="m-0 p-2" key={subscription.id}>
+      <TableCell className="m-0 p-2">{user.name}</TableCell>
+      <TableCell className="m-0 p-2 text-right">{user.company || '(unknown)'}</TableCell>
+      <TableCell className="m-0 p-2 text-left">{subscription.tier.name}</TableCell>
+      <TableCell className="m-0 p-2 text-center"><SubscriptionStatusBadge subscription={subscription}/></TableCell>
+      <TableCell className="m-0 p-2 text-center">{formatDate(subscription.createdAt)}</TableCell>
+      <TableCell className="m-0 p-2 text-right">
+        <div className="flex flex-row justify-end gap-1">
+          <LinkButton label="View" href={`/customers/${user.id}`} />
+        </div>
+      </TableCell>
+    </TableRow>
   );
 };
 
@@ -70,31 +71,19 @@ type ChargeRowProps = {
 };
 
 const ChargeRow = ({ user, charge }: ChargeRowProps) => {
-  const handleRefundCharge = async () => {
-    // Implement the logic to refund the charge
-    console.log('Refunding charge:', charge.id);
-  };
-
   return (
-    <div className="flex max-w-screen-xl flex-col space-y-12">
-    <div className="flex w-full justify-between">
-      <div className="flex flex-col">
-        <PageHeading title="All Customers" />
-        <Text>Manage your customers and their tiers here. </Text>
-      </div>
-    </div>
-    <tr>
-      <td>{user.name}</td>
-      <td>{user.company || '(unknown)'}</td>
-      <td><a href={`mailto:${user.email}`}>{user.email}</a></td>
-      <td>{charge.tier.name}</td>
-      <td>Purchased</td>
-      <td>{formatDate(charge.createdAt)}</td>
-      <td>
-        <button onClick={() => { window.location.href = `/customers/${user.id}` }}>View</button>
-      </td>
-    </tr>
-    </div>
+    <TableRow className="m-0 p-2" key={charge.id}>
+      <TableCell className="m-0 p-2">{user.name}</TableCell>
+      <TableCell className="m-0 p-2 text-right">{user.company || '(unknown)'}</TableCell>
+      <TableCell className="m-0 p-2 text-left">{charge.tier.name}</TableCell>
+      <TableCell className="m-0 p-2 text-center"><PurchaseStatusBadge charge={charge}/></TableCell>
+      <TableCell className="m-0 p-2 text-center">{formatDate(charge.createdAt)}</TableCell>
+      <TableCell className="m-0 p-2 text-right">
+        <div className="flex flex-row justify-end gap-1">
+          <LinkButton label="View" href={`/customers/${user.id}`} />
+        </div>
+      </TableCell>
+    </TableRow>
   );
 };
 
@@ -115,33 +104,39 @@ const CustomersPage = async () => {
   }, [userId]);
 
   return (
-    <div>
-      <h1>Customers</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Customer Name</th>
-            <th>Company</th>
-            <th>Email</th>
-            <th>Tier (Subscription or Purchase)</th>
-            <th>Status</th>
-            <th>Customer Since</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {customers.map((customer) => (
-            <React.Fragment key={customer.id}>
-              {customer.subscriptions.map((subscription) => (
-                <SubscriptionRow key={subscription.id} user={customer} subscription={subscription} />
-              ))}
-              {customer.charges.map((charge) => (
-                <ChargeRow key={charge.id} user={customer} charge={charge} />
-              ))}
-            </React.Fragment>
-          ))}
-        </tbody>
-      </table>
+    <div className="flex max-w-screen-xl flex-col space-y-12">
+      <div className="flex w-full justify-between">
+        <div className="flex flex-col">
+          <PageHeading title="All Customers" />
+          <Text>Manage your customers and their tiers here.</Text>
+        </div>
+      </div>
+      <DashboardCard>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableHeaderCell>Name</TableHeaderCell>
+              <TableHeaderCell className="text-right">Company</TableHeaderCell>
+              <TableHeaderCell className="text-left">Tier</TableHeaderCell>
+              <TableHeaderCell className="text-center">Status</TableHeaderCell>
+              <TableHeaderCell className="text-center">Customer Since</TableHeaderCell>
+              <TableHeaderCell className="text-right"></TableHeaderCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map((customer) => (
+              <React.Fragment key={customer.id}>
+                {customer.subscriptions.map((subscription) => (
+                  <SubscriptionRow key={subscription.id} user={customer} subscription={subscription} />
+                ))}
+                {customer.charges.map((charge) => (
+                  <ChargeRow key={charge.id} user={customer} charge={charge} />
+                ))}
+              </React.Fragment>
+            ))}
+          </TableBody>
+        </Table>
+      </DashboardCard>
     </div>
   );
 };
