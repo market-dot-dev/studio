d351b910176569757ea4a45a84a7985e0f5ef612
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2024-05-27 09:07:33 -0400

    updated demo data
    fyi @gsgoraya

--- components/packages/demo-data.ts
+++ components/packages/demo-data.ts
@@ -1,61 +1,46 @@
 export default [
-	{
-	  "version": "v1.3",
-	  "Dependent Packages": 50
-	},
-	{
-	  "version": "v1.3",
-	  "Dependent Packages": 44
-	},
-	{
-	  "version": "v4.0",
-	  "Dependent Packages": 44
-	},
-	{
-	  "version": "v2.3",
-	  "Dependent Packages": 43
-	},
-	{
-	  "version": "v2.1",
-	  "Dependent Packages": 43
-	},
-	{
-	  "version": "v3.2",
-	  "Dependent Packages": 38
-	},
-	{
-	  "version": "v1.1",
-	  "Dependent Packages": 37
-	},
-	{
-	  "version": "v1.1",
-	  "Dependent Packages": 26
-	},
-	{
-	  "version": "v1.2",
-	  "Dependent Packages": 23
-	},
-	{
-	  "version": "v3.0",
-	  "Dependent Packages": 8
-	},
-	{
-	  "version": "v1.2",
-	  "Dependent Packages": 4
-	},
-	{
-	  "version": "v2.0",
-	  "Dependent Packages": 2
-	},
-	{
-	  "version": "v1.0",
-	  "Dependent Packages": 1
-	},
-	{
-	  "version": "v1.3",
-	  "Dependent Packages": 1
-	}
-  ]
+    {
+        "version": "v1.0",
+        "Dependent Packages": 1
+    },
+    {
+        "version": "v1.1",
+        "Dependent Packages": 4
+    },
+    {
+        "version": "v1.2",
+        "Dependent Packages": 4
+    },
+    {
+        "version": "v1.3",
+        "Dependent Packages": 10
+    },
+    {
+        "version": "v2.0",
+        "Dependent Packages": 18
+    },
+    {
+        "version": "v2.1",
+        "Dependent Packages": 30
+    },
+    {
+        "version": "v2.3",
+        "Dependent Packages": 8
+    },
+    {
+        "version": "v3.0",
+        "Dependent Packages": 40
+    },
+    {
+        "version": "v3.2",
+        "Dependent Packages": 38
+    },
+    {
+        "version": "v4.0",
+        "Dependent Packages": 52
+    }
+]
+
 
   export const dummyFacetsData = {
 	resolved_major_versions: [],
