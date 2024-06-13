caccdca210369a47c13ec84e6e31824c922cbcfa
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2024-06-13 11:35:51 -0400

    new landing page, wip

---
+++ components/common/header.tsx
@@ -0,0 +1,88 @@
+import Link from 'next/link'
+import Dropdown from './dropdown'
+import MobileMenu from './mobile-menu'
+import Image from 'next/image'
+import { Button } from '@tremor/react'
+import DomainService from '@/app/services/domain-service'
+const maintainerLoginUrl = DomainService.getRootUrl('app', '/login');
+
+
+export default function Header() {
+  return (
+    <header className="absolute w-full z-30">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="flex items-center justify-between h-20">
+          {/* Site branding */}
+          <div className="shrink-0 mr-4">
+            {/* Logo */}
+            <Link href="/" className="block">
+              <Image
+                alt="Gitwallet"
+                width={50}
+                height={50}
+                className="relative mx-auto h-12 w-auto"
+                src="/gw-logo-white.png"
+              />            
+            </Link>
+          </div>
+
+          {/* Desktop navigation */}
+          <nav className="hidden md:flex md:grow">
+            {/* Desktop menu links */}
+            <ul className="flex grow justify-end flex-wrap items-center">
+              <li>
+                <Link href="#features" className="text-gray-300 hover:text-gray-200 px-4 py-2 flex items-center transition duration-150 ease-in-out">
+                  Product
+                </Link>
+              </li>
+              <li>
+                <Link href="/pricing" className="text-gray-300 hover:text-gray-200 px-4 py-2 flex items-center transition duration-150 ease-in-out">
+                  Pricing
+                </Link>
+              </li>
+              <li>
+                <Link href="/blog" className="text-gray-300 hover:text-gray-200 px-4 py-2 flex items-center transition duration-150 ease-in-out">
+                  Updates
+                </Link>
+              </li>
+              <li>
+                <Link href="/blog" className="text-gray-300 hover:text-gray-200 px-4 py-2 flex items-center transition duration-150 ease-in-out">
+                  About Us
+                </Link>
+              </li>
+              {/* 1st level: hover */}
+              <Dropdown title="Support">
+                {/* 2nd level: hover */}
+                <li>
+                  <Link href="/contact" className="font-medium text-sm text-gray-400 hover:text-purple-600 flex py-2 px-4 leading-tight">
+                    Join the Discord
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/help/frequently-asked-questions" className="font-medium text-sm text-gray-400 hover:text-purple-600 flex py-2 px-4 leading-tight">
+                    Help Center
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/404" className="font-medium text-sm text-gray-400 hover:text-purple-600 flex py-2 px-4 leading-tight">
+                    Contact Founders
+                  </Link>
+                </li>
+              </Dropdown>
+            </ul>
+
+            {/* Desktop sign in links */}
+            <ul className="flex grow justify-end flex-wrap items-center">
+              <li>
+                <Link href={maintainerLoginUrl}><Button color="gray" variant="secondary" className="w-full">Login →</Button></Link>
+              </li>
+            </ul>
+          </nav>
+
+          <MobileMenu />
+
+        </div>
+      </div>
+    </header>
+  )
+}
