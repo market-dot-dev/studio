549d10b065242beb92cdd8cdd768314a45b0c327
gsgoraya <gsgoraya@gmail.com>
2024-06-14 19:47:40 +0530

    made customer package feature links clickable

--- components/customer/customer-package-features.tsx
+++ components/customer/customer-package-features.tsx
@@ -19,25 +19,14 @@ export default function CustomerPackageFeatures({ features, maintainerEmail }: {
 						<div key={feature.id} className={styles.feature_item + ' flex flex-col gap-2'}>
 							<Title>{feature.name}</Title>
 							<div className="text-sm border rounded-sm p-2 flex justify-between items-center gap-4">
-								<div className="flex flex-col items-stretch grow">
-									<CopyBlock
-										text={feature.uri ?? ''}
-										language='html'
-										theme={a11yLight}
-										/>
-								</div>
-								{!isPhoneNumber(feature.uri!) ?
-									<Button
-										variant="light"
-										onClick={() => window.open(isEmail(feature.uri!) ? `mailto:${feature.uri!}` : feature.uri!, '_blank')}
-										className="text-blue-500 hover:underline focus:outline-none mr-2"
-									>
-										{isEmail(feature.uri!) ? 
-											<svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-black hover:text-gray-400" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m22 2-7 20-4-9-9-4Z"/><path d="M22 2 11 13"/></svg> : 
-											<svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-black hover:text-gray-400" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6"/><path d="m21 3-9 9"/><path d="M15 3h6v6"/></svg>}
-									</Button>		
-								:  <div className="w-4 h-4 mr-2"></div>
-								}
+								
+								{
+								isEmail(feature.uri) ? 
+									<a href={`mailto:${feature.uri}`} className="flex gap-2 items-center"><span>{feature.uri}</span></a> : 
+									isPhoneNumber(feature.uri) ? 
+										<a href={`tel:${feature.uri}`} className="flex gap-2 items-center"><span>{feature.uri}</span></a> : 
+										<a href={feature.uri} target="_blank" className="flex gap-2 items-center"><span>{feature.uri}</span></a>}
+
 							</div>
 						</div>
 						: null
