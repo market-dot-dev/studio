4848fe8570a370ed8934bb822b3d0deb9931262d
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2024-06-18 00:56:28 -0400

    Update mobile-menu.tsx

--- components/common/mobile-menu.tsx
+++ components/common/mobile-menu.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Button } from '@tremor/react'
 import { useState, useRef, useEffect } from 'react'
 import Link from 'next/link'
 
@@ -90,19 +91,11 @@ export default function MobileMenu() {
                 </li> 
             </ul>
           </li>
-          <li>
-            <Link href="/signin" className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center" onClick={() => setMobileNavOpen(false)}>
-              Sign in
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/signup"
-              className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out" onClick={() => setMobileNavOpen(false)}
-            >
-              Sign up
-            </Link>
-          </li>
+          <ul className="flex grow justify-end flex-wrap items-center">
+              <li>
+                <Link href="https://app.gitwallet.co/login"><Button color="green" variant="primary" size="xs" className="w-full">Login →</Button></Link>
+              </li>
+            </ul>
         </ul>
       </nav>
     </div>
