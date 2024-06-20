96ab7ffaf22f528cd1f767d410b92f322a88428b
Aaron Graves <aaron.graves@gmail.com>
2024-06-20 16:29:38 -0400

    fix null crash

--- components/uploader.tsx
+++ components/uploader.tsx
@@ -42,7 +42,7 @@ const UploadPreview = ({
             <path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path>
             <polyline points="13 2 13 9 20 9"></polyline>
           </svg>
-          <p className="mt-2 text-center text-sm text-gray-500">{file!.name}</p>
+          <p className="mt-2 text-center text-sm text-gray-500">{file?.name || '[filename missing]'}</p>
         </div>
       </>
     );
