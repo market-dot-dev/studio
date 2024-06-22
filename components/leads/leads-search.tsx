8c5b31f3433265bae969fd51822550d63d7e8917
gsgoraya <gsgoraya@gmail.com>
2024-06-22 14:02:47 +0530

    removed a debug statement

--- components/leads/leads-search.tsx
+++ components/leads/leads-search.tsx
@@ -416,7 +416,7 @@ export default function LeadsSearch({ repos }: { repos: Repo[] }) {
                         }}
                         onInput={ (e: any) => {
                             setSearchError('');
-                            console.log('this is also called')
+                            
                             setInputRepoUrl(e.target.value);
                         }}
                         onKeyDown={(e: any) => {
