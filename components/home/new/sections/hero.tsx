98ae5e7652ed6dd8beae13655d609e14622c8642
Max <73665502+maxgeor@users.noreply.github.com>
2024-11-04 04:45:40 -0500

    safari logos fix

--- components/home/new/sections/hero.tsx
+++ components/home/new/sections/hero.tsx
@@ -28,48 +28,48 @@ export default function Hero() {
           Sign up with Github
         </Button>
       </div>
-      <div className="xs:-ml-1 xs:gap-3.5 -ml-2 flex items-center gap-3 sm:justify-center sm:gap-4 md:gap-6">
+      <div className="xs:-ml-1 xs:gap-3.5 -ml-2 flex w-fit items-center gap-3 sm:justify-center sm:gap-4 md:gap-6">
         <Image
           src="/new-shipyard-logo.svg"
           alt="shipyard logo"
           height={92}
           width={92}
-          className="h-[clamp(16px,9.1vw,48px)] w-full md:h-12 lg:h-14"
+          className="h-[clamp(16px,9.1vw,48px)] w-auto md:h-12 lg:h-14"
         />
         <Image
           src="/new-qs-logo.svg"
           alt="qs logo"
           height={92}
           width={92}
-          className="h-[clamp(16px,9.1vw,48px)] w-full md:h-12 lg:h-14"
+          className="h-[clamp(16px,9.1vw,48px)] w-auto md:h-12 lg:h-14"
         />
         <Image
           src="/new-arktype-logo.svg"
           alt="arktype logo"
           height={92}
           width={92}
-          className="h-[clamp(16px,9.1vw,48px)] w-full md:h-12 lg:h-14"
+          className="h-[clamp(16px,9.1vw,48px)] w-auto md:h-12 lg:h-14"
         />
         <Image
           src="/new-robyn-logo.svg"
           alt="robyn logo"
           height={92}
           width={92}
-          className="h-[clamp(16px,9.1vw,48px)] w-full md:h-12 lg:h-14"
+          className="h-[clamp(16px,9.1vw,48px)] w-auto md:h-12 lg:h-14"
         />
         <Image
           src="/new-viem-logo.svg"
           alt="viem logo"
           height={92}
           width={126}
-          className="h-[clamp(16px,9.1vw,48px)] w-full md:h-12 lg:h-14"
+          className="h-[clamp(16px,9.1vw,48px)] w-auto md:h-12 lg:h-14"
         />
         <Image
           src="/new-number0-logo.svg"
           alt="number0 logo"
           height={92}
           width={226}
-          className="h-[clamp(16px,9.1vw,48px)] w-full md:h-12 lg:h-14"
+          className="h-[clamp(16px,9.1vw,48px)] w-auto md:h-12 lg:h-14"
         />
       </div>
       <div className="xs:rounded-md -mr-60 h-full w-[calc(100vw+192px)] overflow-visible rounded ring-1 ring-black/[8%] sm:rounded-lg md:-mr-72 md:mt-0 lg:mx-auto lg:mt-0 lg:w-full lg:max-w-[1150px] lg:rounded-xl">
