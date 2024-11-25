fd3c18c258ace257313771f603a6e81b6f2023b5
Max <73665502+maxgeor@users.noreply.github.com>
2024-11-25 11:10:33 -0500

    final design for business info wizard step

--- app/design/wizard/business/page.tsx
+++ app/design/wizard/business/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { TextInput, Button } from "@tremor/react";
-import { UsersRound, UserRound, Image as ImageIcon } from "lucide-react";
+import { UsersRound, UserRound, ImageIcon } from "lucide-react";
 import { useState, useRef } from "react";
 
 export default function OnboardingForm() {
@@ -20,7 +20,7 @@ export default function OnboardingForm() {
   };
 
   return (
-    <div className="w-full max-w-lg space-y-8">
+    <div className="w-full max-w-lg flex flex-col gap-8 ">
       <div className="flex justify-center">
         <Image
           src="/gw-logo-nav.png"
@@ -32,40 +32,40 @@ export default function OnboardingForm() {
       </div>
 
       <div className="space-y-2 text-center">
-        <h1 className="text-4xl font-semibold tracking-tight text-stone-900">
+        <h1 className="text-4xl font-semibold tracking-tight text-gray-900">
           Welcome to Gitwallet
         </h1>
-        <h2 className="text-2xl font-normal text-stone-900">
+        <h2 className="text-2xl font-normal text-gray-900">
           Tell us about your business
         </h2>
       </div>
 
-      <div className="space-y-6">
+      <div className="space-y-8">
         <div className="space-y-2">
-          <label className="block text-stone-900">Business Name</label>
-          <TextInput
-            placeholder=""
-            className="border-0 bg-white shadow-sm ring-1 ring-black/10"
-          />
+          <label className="block text-gray-900">Business Name</label>
+          <TextInput placeholder="" className="bg-white" />
         </div>
 
         <div className="space-y-2">
-          <label className="block text-stone-900">Domain</label>
-          <TextInput
-            placeholder=""
-            className="border-0 bg-white shadow-sm ring-1 ring-black/10"
-          />
-          <p className="text-sm text-stone-500">
+          <label className="block text-gray-900">Domain</label>
+          <div className="flex items-center justify-between gap-4 rounded-tremor-default border border-tremor-border bg-white shadow-tremor-input">
+            <TextInput
+              placeholder=""
+              className="rounded-r-none border-none bg-white shadow-none focus:border focus:border-gray-900"
+            />
+            <span className="py-2 pr-3 text-gray-400 text-sm">.gitwallet.co</span>
+          </div>
+          <p className="text-xs text-gray-500">
             Your landing page will live here (you can change this later).
           </p>
         </div>
 
         <div className="space-y-2">
-          <label className="flex items-baseline justify-between text-stone-900">
+          <label className="flex items-baseline justify-between text-gray-900">
             Logo
-            <span className="ml-2 text-xs text-stone-500">Optional</span>
+            <span className="ml-2 text-xs text-gray-500">Optional</span>
           </label>
-          <div className="rounded-lg border border-dashed border-stone-300 bg-stone-100 p-10 text-center">
+          <div className="rounded-lg border border-dashed border-gray-300 bg-gray-100 p-10 text-center">
             <input
               type="file"
               ref={fileInputRef}
@@ -74,8 +74,8 @@ export default function OnboardingForm() {
               accept="image/*"
             />
             <div className="mx-auto flex flex-col items-center">
-              <ImageIcon className="mx-auto h-6 w-6 text-stone-400" />
-              <div className="mt-3 text-xs text-stone-500">
+              <ImageIcon className="mx-auto h-6 w-6 text-gray-400" />
+              <div className="mt-3 text-xs text-gray-500">
                 <p>Drag & drop a .png or .jpg</p>
                 <p>
                   or{" "}
@@ -88,50 +88,47 @@ export default function OnboardingForm() {
                 </p>
               </div>
               {file && (
-                <p className="mt-2 text-stone-600">Selected: {file.name}</p>
+                <p className="mt-2 text-gray-600">Selected: {file.name}</p>
               )}
             </div>
           </div>
         </div>
 
         <div className="space-y-2">
-          <label className="block text-stone-900">
+          <label className="block text-gray-900">
             Where are you based out of?
           </label>
-          <TextInput
-            placeholder="Toronto, Canada"
-            className="border-0 bg-white shadow-sm ring-1 ring-black/10"
-          />
+          <TextInput placeholder="Toronto, Canada" className="bg-white" />
         </div>
 
         <div className="space-y-2">
-          <label className="block text-stone-900">
+          <label className="block text-gray-900">
             Are you a team or independent?
           </label>
           <div className="space-y-2">
-            <label className="block w-full">
-              <div className="flex cursor-pointer items-center justify-between rounded-tremor-default bg-white p-3 shadow-sm ring-1 ring-black/10 hover:bg-stone-50">
+            <label className="block w-full rounded-tremor-default focus-within:outline-none focus-within:ring-2 focus-within:ring-gray-200">
+              <div className="flex cursor-pointer items-center justify-between rounded-tremor-default border bg-white p-3 shadow-sm [&:has(input:checked)]:border-gray-800 [&:has(input:checked)]:ring-1 [&:has(input:checked)]:ring-gray-800">
                 <div className="flex items-center">
-                  <UsersRound className="mr-3 h-5 w-5 text-stone-500" />
-                  <span className="text-stone-900">We&apos;re a team</span>
+                  <UsersRound className="mr-3 h-5 w-5 text-gray-500" />
+                  <span className="text-gray-900">We&apos;re a team</span>
                 </div>
                 <input
                   type="radio"
                   name="team-type"
-                  className="text-stone-500 checked:text-stone-900"
+                  className="text-gray-500 checked:text-gray-900 focus:outline-none"
                 />
               </div>
             </label>
-            <label className="block w-full">
-              <div className="flex cursor-pointer items-center justify-between rounded-tremor-default bg-white p-3 shadow-sm ring-1 ring-black/10 hover:bg-stone-50">
+            <label className="block w-full rounded-tremor-default focus-within:outline-none focus-within:ring-2 focus-within:ring-gray-200">
+              <div className="flex cursor-pointer items-center justify-between rounded-tremor-default border bg-white p-3 shadow-sm [&:has(input:checked)]:border-gray-800 [&:has(input:checked)]:ring-1 [&:has(input:checked)]:ring-gray-800">
                 <div className="flex items-center">
-                  <UserRound className="mr-3 h-5 w-5 text-stone-500" />
-                  <span className="text-stone-900">It&apos;s just me</span>
+                  <UserRound className="mr-3 h-5 w-5 text-gray-500" />
+                  <span className="text-gray-900">It&apos;s just me</span>
                 </div>
                 <input
                   type="radio"
                   name="team-type"
-                  className="text-stone-500 checked:text-stone-900"
+                  className="text-gray-500 checked:text-gray-900 "
                   defaultChecked
                 />
               </div>
@@ -141,11 +138,11 @@ export default function OnboardingForm() {
       </div>
 
       {/* Navigation */}
-      <div className="flex justify-between pt-6">
-        <Button variant="light" className="text-stone-600">
+      <div className="flex justify-end pt-6">
+        {/* <Button variant="light" className="text-gray-600">
           Back
-        </Button>
-        <Button className="bg-stone-900 text-white hover:bg-stone-800">
+        </Button> */}
+        <Button className="bg-gray-900 text-white hover:bg-gray-800">
           Next
         </Button>
       </div>
--- app/design/wizard/layout.tsx
+++ app/design/wizard/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function WizardLayout({ children }: { children: React.ReactNode}) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center text-stone-450 bg-stone-100 px-6 py-12 font-sans font-bold text-base">
+    <div className="flex min-h-screen flex-col items-center justify-center text-gray-400 bg-gray-100 px-6 py-12 font-sans font-bold text-base">
       {children}
     </div>
   );
