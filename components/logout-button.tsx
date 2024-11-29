3f593224f81b085a32ff7aa94daa626c699492a1
Max <73665502+maxgeor@users.noreply.github.com>
2024-11-29 13:20:43 -0500

    spacing and style updates to sidebar

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -9,9 +9,9 @@ export default function LogoutButton({ redirect }: { redirect: string}) {
       onClick={() => signOut(
         { callbackUrl: redirect }
       )}
-      className="rounded-lg p-1.5 text-stone-700 transition-all duration-150 ease-in-out hover:bg-stone-200 active:bg-stone-300 dark:text-white dark:hover:bg-stone-700 dark:active:bg-stone-800"
+      className="rounded p-1 text-stone-600 transition-all duration-150 ease-in-out hover:bg-stone-200 active:bg-stone-300 dark:text-white dark:hover:bg-stone-700 dark:active:bg-stone-800"
     >
-      <LogOut width={18} />
+      <LogOut size={18} />
     </button>
   );
 }
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import {
-  Globe,
-  KanbanSquare,
+  AppWindowMac,
   Scroll,
   Menu,
-  Users,
+  UsersRound,
   Settings,
-  BarChart4,
-  Code2,
-  Radar,
-  Box,
+  ChartNoAxesColumn,
+  CodeSquare,
+  ScanSearch,
   Home,
-  Eye,
+  Package,
+  Box,
 } from "lucide-react";
 import {
   useParams,
@@ -21,146 +21,220 @@ import {
   useSelectedLayoutSegments,
 } from "next/navigation";
 import { ReactNode, useEffect, useMemo, useState } from "react";
-import { GearIcon } from "@radix-ui/react-icons";
 import { FaDiscord, FaGithubAlt, FaTelegramPlane } from "react-icons/fa";
 import { Badge } from "@tremor/react";
 
+type TabTitle = {
+  type: 'title';
+  name: string;
+};
+
+type TabLink = {
+  type: 'link';
+  name: string;
+  href: string;
+  icon: ReactNode;
+  target?: "_blank" | "_self";
+  isActive?: boolean;
+  isBeta?: boolean;
+};
+
+type TabMenu = {
+  type: 'menu'
+  name: string;
+  icon: ReactNode;
+  links: TabLink[];
+};
+
+type TabItem = TabTitle | TabLink | TabMenu;
+
+const Tab: React.FC<TabItem> = (props) => {
+  if (props.type === 'title') {
+    const { name } = props;
+
+    return (
+      <span
+        key={name}
+        className="font-small ml-1 mt-4 text-xs font-medium uppercase leading-6 tracking-wide text-stone-500"
+      >
+        {name}
+      </span>
+    )
+  }
+
+  if (props.type === 'link') {
+    const { name, href, icon, target, isActive, isBeta } = props;
+
+    return (
+      <Link
+        key={name}
+        href={href}
+        target={target || '_self'}
+        className={`flex items-center gap-2 ${
+          isActive ? "bg-stone-200" : ""
+        } rounded px-1 py-0.5 transition-all duration-150 ease-in-out hover:bg-stone-200 active:bg-stone-300 dark:text-white dark:hover:bg-stone-700 dark:active:bg-stone-800`}
+      >
+        <span className="mx-px">{icon}</span>
+        <span className="text-sm font-medium">{name}</span>
+        {isBeta && (
+          <Badge className="rounded border-none border-stone-200 bg-stone-50 px-1 !text-xxs font-semibold uppercase tracking-wide text-stone-600">
+            Beta
+          </Badge>
+        )}
+      </Link>
+    );
+  }
+
+  return null;
+};
+
 export default function Nav({ children, siteId, roleId, hasFeatures }: { children: ReactNode, siteId: string | null, roleId: string | null, hasFeatures: boolean | null}) {
   const urlSegments = useSelectedLayoutSegments();
-  const { id } = useParams() as { id?: string };
 
-  const tabs = useMemo(() => {
-    return [
+  const tabs: TabItem[] = useMemo(() => {
+    const tabItems: TabItem[] = [
       {
+        type: "link",
         name: "Home",
         href: "/",
         isActive: urlSegments.length === 0,
-        icon: <Home width={18} />,
-      }, 
-
+        icon: <Home size={18} />,
+      },
       {
+        type: "link",
         name: "Settings",
         href: "/settings",
         isActive: urlSegments[0] === "settings",
-        icon: <Settings width={18} />,
+        icon: <Settings size={18} />,
       },
+      {
+        type: "title",
+        name: "Offerings",
+      },
+    ];
 
-      // Services
+    if (hasFeatures) {
+      tabItems.push({
+        type: "link",
+        name: "Services",
+        href: "/features",
+        isActive: urlSegments[0] === "features",
+        icon: <Box size={18} />,
+      });
+    }
+
+    tabItems.push(
       {
-        name: "Your Services",
-        href: "",
-        isDivider: true,
-      },
-      ...(hasFeatures ? [
-        {
-          name: "Services",
-          href: "/features",
-          isActive: urlSegments[0] === "features",
-          icon: <Box width={18} />,
-      }] : []),  
-      {
+        type: "link",
         name: "Packages",
         href: "/tiers",
         isActive: urlSegments[0] === "tiers",
-        icon: <KanbanSquare width={18} />,
+        icon: <Package size={18} />,
       },
       {
+        type: "link",
         name: "Contracts",
         href: "/contracts",
         isActive: urlSegments[1] === "contracts",
         isBeta: true,
-        icon: <Scroll width={18} />,
-      },
-  
-      // Customers
-      {
-        name: "Customers",
-        href: "",
-        isDivider: true,
+        icon: <Scroll size={18} />,
       },
       {
-        name: "Customers",
-        href: "/customers",
-        isActive: urlSegments[0] === "customers",
-        icon: <Users width={18} />,
-      },
-      {
-        name: "Research",
-        href: "/leads",
-        isActive: urlSegments[0] === "leads",
-        icon: <Radar width={18} />,
-      },
-
-      // Marketing
-      {
+        type: "title",
         name: "Marketing",
-        href: "",
-        isDivider: true,
       },
-      ...(siteId ?
-        [{
-          name: "Your Site",
-          href: `/site/${siteId}`,
-          isActive: urlSegments[0] === "site" || urlSegments[0] === "page",
-          icon: <Globe width={18} />
-        }] : []),
+    );
+
+    if (siteId) {
+      tabItems.push({
+        type: "link",
+        name: "Website",
+        href: `/site/${siteId}`,
+        isActive: urlSegments[0] === "site" || urlSegments[0] === "page",
+        icon: <AppWindowMac size={18} />,
+      });
+    }
+
+    tabItems.push(
       {
+        type: "link",
         name: "Embeds",
         href: "/channels/embeds",
         isActive: urlSegments[0] === "embeds",
-        icon: <Code2 width={18} />,
+        icon: <CodeSquare size={18} />,
       },
-
-      // Analytics 
       {
+        type: "title",
+        name: "Customers",
+      },
+      {
+        type: "link",
+        name: "Customers",
+        href: "/customers",
+        isActive: urlSegments[0] === "customers",
+        icon: <UsersRound size={18} />,
+      },
+      {
+        type: "link",
+        name: "Research",
+        href: "/leads",
+        isActive: urlSegments[0] === "leads",
+        icon: <ScanSearch size={18} />,
+      },
+      {
+        type: "title",
         name: "Analytics",
-        href: "",
-        isDivider: true,
       },
       {
+        type: "link",
         name: "Reports",
         href: "/reports",
         isActive: urlSegments[0] === "reports",
-        icon: <BarChart4 width={18} />,
+        icon: <ChartNoAxesColumn size={18} />,
       },
-      // SUPPORT
-      {
-        name: "Get Support",
-        href: "",
-        isDivider: true,
-      },
-      {
-        name: "Join Discord",
-        href: "https://discord.gg/ZdSpS4BuGd",
-        target: "_blank",
-        icon: <FaDiscord width={18} />,
-      },
-      {
-        name: "DM Founder",
-        href: "https://t.me/tarunsachdeva2",
-        target: "_blank",
-        icon: <FaTelegramPlane width={18} />,
-      },
-      {
-        name: "Github",
-        href: "https://www.github.com/git-wallet",
-        target: "_blank",
-        icon: <FaGithubAlt width={18} />,
-      },
-      ...(['admin'].includes(roleId || '') ?
-        [{
+    );
+
+    if (roleId === "admin") {
+      tabItems.push(
+        {
+          type: "title",
           name: "⚠️ DEBUG MENU ⚠️",
-          href: "",
-          isDivider: true,
         },
         {
+          type: "link",
           name: "Debug",
           href: `/admin/debug`,
-          icon: <GearIcon width={18} />,
-        }
-        ] : []),
-    ];
-  }, [urlSegments, id, siteId, roleId]);
+          icon: <Settings size={18} />,
+        },
+      );
+    }
+
+    return tabItems;
+  }, [urlSegments, siteId, roleId, hasFeatures]);
+
+  const supportLinks: TabLink[] = [
+    {
+      type: "link",
+      name: "Join Discord",
+      href: "https://discord.gg/ZdSpS4BuGd",
+      target: "_blank",
+      icon: <FaDiscord size={18} />,
+    },
+    {
+      type: "link",
+      name: "DM Founder",
+      href: "https://t.me/tarunsachdeva2",
+      target: "_blank",
+      icon: <FaTelegramPlane size={18} />,
+    },
+    {
+      type: "link",
+      name: "Github",
+      href: "https://www.github.com/git-wallet",
+      target: "_blank",
+      icon: <FaGithubAlt size={18} />,
+    },
+  ];
 
   const [showSidebar, setShowSidebar] = useState(false);
 
@@ -179,44 +253,41 @@ export default function Nav({ children, siteId, roleId, hasFeatures }: { childre
           urlSegments[0] === "post" && urlSegments.length === 2 && !showSidebar
             ? "left-5 top-5"
             : "right-5 top-7"
-          } sm:hidden`}
+        } sm:hidden`}
         onClick={() => setShowSidebar(!showSidebar)}
       >
         <Menu width={20} />
       </button>
       <div
-        className={`transform ${showSidebar ? "w-full translate-x-0" : "-translate-x-full"
-          } fixed z-20 flex h-full flex-col justify-between border-r border-stone-200 bg-stone-100 p-4 transition-all dark:border-stone-700 dark:bg-stone-900 sm:w-60 sm:translate-x-0`}
+        className={`transform ${
+          showSidebar ? "w-full translate-x-0" : "-translate-x-full"
+        } fixed z-20 flex h-full flex-col justify-between bg-stone-100 p-4 pl-3 pt-3.5 transition-all dark:border-stone-700 dark:bg-stone-900 sm:w-60 sm:translate-x-0`}
       >
-        <div className="grid gap-2">
-          <div className="flex items-center space-x-2 rounded-lg py-1.5">
-            <div className="text-md font-medium">
-              <Link href="/" ><img src="/gw-logo-nav.png" className="h-8 hover:scale-110" /></Link>
-            </div>
-          </div>
-          <div className="grid gap-0.5">
-            {tabs.map(({ name, href, target, isActive, isBeta, icon }, index: number) => (
-              href === "" ? (
-                <span key={name} className="text-xs font-small uppercase mt-4">{name}</span>
-              ) : (
-                <Link
-                  key={name + index}
-                  href={href}
-                  target={target}
-                  className={`flex items-center space-x-3 ${isActive ? "bg-stone-200 text-black dark:bg-stone-700" : ""
-                    } rounded-lg px-1 transition-all duration-150 ease-in-out hover:bg-stone-200 active:bg-stone-300 dark:text-white dark:hover:bg-stone-700 dark:active:bg-stone-800`}
-                >
-                  {icon}
-                  <span className="text-sm font-medium">{name}</span>
-                  {isBeta && <Badge size={'xs'} tooltip="This feature is still in Beta">Beta</Badge>}
-                </Link>
-              )
+        <div className="grid gap-4">
+          <Link href="/" className="ml-0.5">
+            <Image
+              src="/gw-logo-nav.png"
+              alt="Gitwallet logo"
+              height={24}
+              width={24}
+              className="h-6 w-6"
+            />
+          </Link>
+          <div className="flex flex-col">
+            {tabs.map((tab) => (
+              <Tab key={tab.name} {...tab} />
             ))}
           </div>
         </div>
-        <div>
-          <div className="my-2 border-t border-stone-200 dark:border-stone-700" />
-          {children}
+        <div className="flex flex-col gap-5 ">
+          <div className="flex flex-col">
+            {supportLinks.map((link) => (
+              <Tab key={link.name} {...link} />
+            ))}
+          </div>
+          <div className="ml-0.5">
+            {children}
+          </div>
         </div>
       </div>
     </>
