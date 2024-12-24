664b92182615f7b4b4e59d9618812feb2f9d7381
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2024-12-24 16:37:56 -0500

    updated text for privacy policy and TOS

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,90 +1,98 @@
 export default function PrivacyPolicy() {
-  return (
-    <div className="mx-auto max-w-5xl px-6 py-16">
-        <h1 className="text-marketing-primary text-marketing-base text-pretty font-bold tracking-tight ">Privacy Policy</h1>
+    return (
+      <div className="mx-auto max-w-5xl px-6 py-16">
+        <h1 className="text-marketing-primary text-marketing-base text-pretty font-bold tracking-tight">Privacy Policy</h1>
         <p className="text-marketing-secondary mb-4">Effective as of December 1, 2024</p>
-        
+  
         <div className="space-y-6 text-marketing-secondary">
           <p>
-            Gitwallet is committed to the highest standards of privacy and security to ensure that any personal information you entrust to us when using our Services is fully protected. This policy sets out our practices when collecting, using and disclosing any of your information in the course of providing you Services through the Gitwallet Platform ("Privacy Policy").
+            Gitwallet is committed to the highest standards of privacy and security to ensure that any personal information you entrust to us when using our Services is fully protected.&nbsp; This policy sets out our practices when collecting, using and disclosing any of your information in the course of providing you Services through the Gitwallet Platform (<strong>“Privacy Policy”</strong>).
           </p>
-
+  
           <h2 className="text-marketing-primary font-bold tracking-[-0.03em]">Information we collect and use in delivery Services</h2>
+  
           <p>
-            When you set up an account with us, we collect your identification and contact information, such as first name, last name, telephone number and email address, and we use this information when providing Services to you, including when using and communicating through the Gitwallet Platform and accessing customer support. The purposes for which we use your information, include, but may not be limited to, enabling and supporting:
+            When you set up an account with us, we collect your identification and contact information, such as first name, last name, telephone number and email address, and we use this information when providing Services to you, including when using and communicating through the Gitwallet Platform and accessing customer support.&nbsp; The purposes for which we use your information, include, but may not be limited to, enabling and supporting:
           </p>
-          
-          <ul>
+  
+          <ul className="list-disc list-inside space-y-2">
             <li>
-              <strong className="text-marketing-primary">Business Operations:</strong> billing and accounting, including collecting the last four digits of your credit card number for these purposes
+              <strong className="text-marketing-primary">Business Operations:</strong> billing and accounting, including collecting the last four digits of your credit card number for these purposes;
             </li>
             <li>
-              <strong className="text-marketing-primary">Communication:</strong> informing you about new Services, features, offers, promotions, and other pertinent information
+              <strong className="text-marketing-primary">Communication:</strong> informing you about new Services, features, offers, promotions, and other pertinent information;
             </li>
             <li>
-              <strong className="text-marketing-primary">Service Provision:</strong> in delivering and updating our Services as configured and used by you, and to make ongoing personalized experiences and recommendations
+              <strong className="text-marketing-primary">Service Provision:</strong> in delivering and updating our Services as configured and used by you, and to make ongoing personalized experiences and recommendations; and
             </li>
             <li>
-              <strong className="text-marketing-primary">Ongoing Service Performance:</strong> keeping the Services up-to-date and performant, and meeting user productivity, reliability, efficacy, quality, privacy, accessibility, and security needs
+              <strong className="text-marketing-primary">Ongoing Service Performance:</strong> keeping the Services up-to-date and performant, and meeting user productivity, reliability, efficacy, quality, privacy, accessibility, and security needs.
             </li>
           </ul>
-
+  
           <h2 className="text-marketing-primary font-bold tracking-[-0.03em]">International transfers</h2>
           <p>
-            In order to deliver Services to you, we may rely on technology that stores or processes your information outside the jurisdiction in which you reside. As such, your information may become subject to the laws of another jurisdiction, which may differ from the laws of your jurisdiction. By using our Services, you consent to the transfer of your information outside of your jurisdiction when necessary for delivering the Services.
+            In order to deliver Services to you, we may rely on technology that stores or processes your information outside the jurisdiction in which you reside.&nbsp; As such, your information may become subject to the laws of another jurisdiction, which may differ from the laws of your jurisdiction. By using our Services, you consent to the transfer of your information outside of your jurisdiction when necessary for delivering the Services.
           </p>
-
+  
           <h2 className="text-marketing-primary font-bold tracking-[-0.03em]">Privacy Rights</h2>
           <p>
             Subject to certain limitations and depending on the privacy laws applicable to you, you may have the following legal rights to your information:
           </p>
-          <ul>
-            <li>Right to access or correct your information held by us</li>
-            <li>The right to object to the processing of your information, as allowed by applicable law</li>
+          <ul className="list-disc list-inside space-y-2">
+            <li>Right to access or correct your information held by us;</li>
+            <li>The right to object to the processing of your information, as allowed by applicable law; and</li>
           </ul>
-
+  
           <p>
-don            Where we rely on your consent to process your information, you may have the right to withdraw or decline your consent at any time and, where we rely on legitimate interests, you have the right to object (as noted above). Please note that withdrawing your consent may affect your access and ability to use our Services.
+            Where we rely on your consent to process your information, you may have the right to withdraw or decline your consent at any time and, where we rely on legitimate interests, you have the right to object (as noted above).&nbsp; Please note that withdrawing your consent may affect your access and ability to use our Services.
           </p>
-
+  
           <p>
             You have the right to file a complaint about the way we use or process your information with the Information and Privacy Commissioner of Ontario (Canada) or other data protection authority as may be applicable to your jurisdiction.
           </p>
-
+  
           <p>
-            Should you need to access or correct your personal information that is held by us, please use the contact information in the "Contact Us" section at the end of this policy to reach us.
+            Should you need to access or correct your personal information that is held by us, please use the contact information in the <strong>“Contact Us”</strong> section at the end of this policy to reach us.
           </p>
-
+  
           <h2 className="text-marketing-primary font-bold tracking-[-0.03em]">Safeguarding your information</h2>
           <p>
             Gitwallet uses industry-leading technical and organizational security measures to protect your information held by us from unauthorized access, disclosure, modification, and accidental loss and destruction.
           </p>
-
+  
           <h2 className="text-marketing-primary font-bold tracking-[-0.03em]">Data Retention</h2>
           <p>
             Gitwallet retains personal information only as long as necessary to provide and improve the Services or to meet legal obligations. Your information retained by Gitwallet will be de-identified, aggregated, and/or securely deleted once it is no longer needed for such purposes.
           </p>
-
+  
           <h2 className="text-marketing-primary font-bold tracking-[-0.03em]">Cookies and other similar technologies</h2>
           <p>
-            Gitwallet uses cookies and similar tracking technologies on our website and the Gitwallet Platform to enhance user experience. Should you have any questions about how we use cookies, please use the contact information in the "Contact Us" section at the end of this policy to reach us.
+            Gitwallet uses cookies and similar tracking technologies on our website and the Gitwallet Platform to enhance user experience.&nbsp; Should you have any questions about how we use cookies, please use the contact information in the <strong>“Contact Us”</strong> section at the end of this policy to reach us.
           </p>
-
+  
           <h2 className="text-marketing-primary font-bold tracking-[-0.03em]">Third Party links</h2>
           <p>
             The Gitwallet Platform may contain links or enable maintainers to link to other websites that are not under our control and we assume no responsibility for those links or the privacy practices of the websites should you choose to navigate to them.
           </p>
-
+  
           <h2 className="text-marketing-primary font-bold tracking-[-0.03em]">Updates</h2>
           <p>
             We may update this Privacy Policy from time to time to reflect changes to how we handle your information held by us. Your continued use of our website, the Gitwallet Platform or Services will be deemed acceptance by you of the updated Privacy Policy.
           </p>
-
+  
           <h2 className="text-marketing-primary font-bold tracking-[-0.03em]">Contact Us</h2>
           <p>
-            If you have any questions or concerns about our privacy practices or this policy, you may contact us by email: <a href="mailto:privacy@gitwallet.co" className="text-marketing-primary hover:text-marketing-primary/90">privacy@gitwallet.co</a>
+            If you have any questions or concerns about our privacy practices or this policy, you may contact us by email:&nbsp;
+            <a
+              href="mailto:contact@lab0324.xyz"
+              className="text-marketing-primary hover:text-marketing-primary/90"
+            >
+              contact@lab0324.xyz
+            </a>.
           </p>
         </div>
-    </div>
-  );
-} 
+      </div>
+    );
+  }
+  
