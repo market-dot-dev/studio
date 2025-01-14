9c1aacdfe8cf2180e423fbf2c3978f7e843c81f7
Mathusan Selvarajah <mathusans52@gmail.com>
2025-01-13 21:17:19 -0500

    add extra reserved subdomains

--- middleware.ts
+++ middleware.ts
@@ -25,29 +25,29 @@ export default withAuth(
     callbacks: {
       authorized: async ({ token, req }) => {
         const user = token?.user as SessionUser;
-        return await RoleService.canViewPath(req.nextUrl.pathname, user?.roleId as Role);
-      }
+        return await RoleService.canViewPath(
+          req.nextUrl.pathname,
+          user?.roleId as Role,
+        );
+      },
     },
-  }
+  },
 );
 
 const rewrite = (path: string, url: string) => {
   return NextResponse.rewrite(new URL(path, url));
-}
+};
 
 async function customMiddleware(req: NextRequest) {
   const url = req.nextUrl;
 
-  const hostname = DomainService.getHostnameFromRequest(req);
   const rootUrl = DomainService.getRootUrl();
 
-  const ghUsername = DomainService.getGhUsernameFromRequest(req);``
+  const ghUsername = DomainService.getGhUsernameFromRequest(req);
   const reservedSubdomain = DomainService.getReservedSubdomainFromRequest(req);
-  const bareDomain = !ghUsername && !reservedSubdomain;``
-  const session = await getToken({ req }) as any;
-  const signedIn = !!session;
+  const bareDomain = !ghUsername && !reservedSubdomain;
+  const session = (await getToken({ req })) as any;
   const roleId = session?.user?.roleId;
-  const hostUrl = DomainService.getRootUrlFromRequest(req)
 
   const searchParams = req.nextUrl.searchParams.toString();
 
@@ -57,31 +57,34 @@ async function customMiddleware(req: NextRequest) {
   path = path === "/" ? "" : path;
 
   // exempt from middleware rewrites
-  if (url.pathname.startsWith('/monitoring') || url.pathname.startsWith('/design')) {
+  if (
+    url.pathname.startsWith("/monitoring") ||
+    url.pathname.startsWith("/design")
+  ) {
     return NextResponse.next();
   }
 
   // gitwallet.co
-  if (bareDomain || reservedSubdomain === 'sell') {
-    if (url.pathname.startsWith('/design')) {
+  if (bareDomain || reservedSubdomain === "sell") {
+    if (url.pathname.startsWith("/design")) {
       return rewrite(`/design${path}`, req.url);
     }
-    if (url.pathname === '/terms' || url.pathname === '/privacy') {
+    if (url.pathname === "/terms" || url.pathname === "/privacy") {
       return NextResponse.next();
     }
     return rewrite(`/home${path}`, req.url);
   }
 
   // $GHUSERNAME.gitwallet.co
-    // permit API from users' subdomains
-  if(!!ghUsername) {
-    if( url.pathname.startsWith('/api')) {
+  // permit API from users' subdomains
+  if (!!ghUsername) {
+    if (url.pathname.startsWith("/api")) {
       return NextResponse.next();
     }
-    
+
     return rewrite(`/maintainer-site/${ghUsername}${path}`, req.url);
   }
-  
+
   // *.gitwallet.co
   const loginPaths = ["/login", "/customer-login", "/login/local-auth"];
 
@@ -91,24 +94,29 @@ async function customMiddleware(req: NextRequest) {
   }
 
   // app.gitwallet.co
-  if (reservedSubdomain === 'app' || DomainService.isVercelPreview(req)) {
+  if (reservedSubdomain === "app" || DomainService.isVercelPreview(req)) {
     // if customer, then lock to /app/c/
 
-    if(roleId === 'customer') {
-      if(url.pathname.startsWith('/checkout') || url.pathname.startsWith('/success')) {
+    if (roleId === "customer") {
+      if (
+        url.pathname.startsWith("/checkout") ||
+        url.pathname.startsWith("/success")
+      ) {
         return rewrite(`/app${path}`, req.url);
       } else {
         return rewrite(`/app/c${path}`, req.url);
       }
     } else {
-      if(url.pathname.startsWith('/charges') || url.pathname.startsWith('/subscriptions') || url.pathname.startsWith('/packages')) {
-        return NextResponse.redirect(
-          `${rootUrl}/c${path}`,
-        );
+      if (
+        url.pathname.startsWith("/charges") ||
+        url.pathname.startsWith("/subscriptions") ||
+        url.pathname.startsWith("/packages")
+      ) {
+        return NextResponse.redirect(`${rootUrl}/c${path}`);
       }
     }
 
-    if( url.pathname.startsWith('/api')) {
+    if (url.pathname.startsWith("/api")) {
       return NextResponse.next();
     }
 
