7881c70f827550b89b84dd2a775836ec55d323be
Max <73665502+maxgeor@users.noreply.github.com>
2025-03-07 15:43:44 -0500

    auth component updates

--- app/app/(auth)/login/page.tsx
+++ app/app/(auth)/login/page.tsx
@@ -23,17 +23,17 @@ export default async function LoginPage({
         alt="market.dev logo"
         width={64}
         height={64}
-        className="relative mx-auto h-14 w-14"
+        className="relative mx-auto h-10 w-10"
         src="/gw-logo-nav.png"
       />
-      <h1 className="mt-8 text-center text-4xl font-bold tracking-tight dark:text-white">
+      <h1 className="mt-4 text-center text-2xl font-bold tracking-tightish dark:text-white">
         Login to market.dev
       </h1>
-      <p className="mt-3 text-center text-sm text-stone-600 dark:text-stone-400">
-        All-in-One Storefronts, built for Developers.
+      <p className="mt-3 text-center text-sm text-stone-500 dark:text-stone-400">
+        All-in-one business tools, built for developers.
       </p>
 
-      <div className="mx-auto mt-8 flex w-full max-w-sm flex-col gap-2">
+      <div className="mx-auto mt-6 flex w-full max-w-xs flex-col gap-2">
         <Suspense
           fallback={
             <div className="h-12 w-full rounded-md border border-stone-200 bg-stone-100 dark:border-stone-700 dark:bg-stone-800" />
--- components/common/login-button.tsx
+++ components/common/login-button.tsx
@@ -3,25 +3,37 @@
 import { useRouter } from "next/navigation";
 import { ReactNode, MouseEventHandler, FC } from "react";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 interface LoginButtonProps {
   children: ReactNode;
   onClick?: MouseEventHandler<HTMLButtonElement>;
   isLoading: boolean;
   href?: string;
+  loadingText?: string;
+  className?: string;
 }
 
 export const LoginButton: FC<LoginButtonProps> = ({
   children,
   onClick,
   isLoading,
+  loadingText,
   href,
+  className = ""
 }) => {
   const router = useRouter();
   const loginOrNavigate = href ? () => router.push(href) : onClick;
 
   return (
-    <Button variant="outline" disabled={isLoading} loading={isLoading} onClick={loginOrNavigate}>
+    <Button
+      variant="outline"
+      disabled={isLoading}
+      loading={isLoading}
+      loadingText={loadingText}
+      onClick={loginOrNavigate}
+      className={className}
+    >
       {children}
     </Button>
   );
