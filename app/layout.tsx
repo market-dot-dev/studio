382583d1fda7b4fdf4a5fe98c117da80ceee2002
Thomas Gram <thomas@codeshark.net>
2025-04-10 14:43:38 +0200

    Remove legacy fonts definition.

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* @TODO: Nooooo, this should be internal, you don't want to include the whole thing. */}
         <link href="https://cdn.tailwindcss.com" rel="stylesheet" />
         {/* <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries"></script> */}
       </head>
