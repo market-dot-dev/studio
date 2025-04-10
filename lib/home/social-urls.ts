8fb58964af935132610020751bea69c1605db3aa
Thomas Gram <thomas@codeshark.net>
2025-04-10 14:55:36 +0200

    Cleanup old migration endpoint. Add comment.

--- lib/home/social-urls.ts
+++ lib/home/social-urls.ts
@@ -1,8 +1,10 @@
+// @TODO: This shouldn't be defined here? This is an env var.
 const loginURL =
   process.env.NODE_ENV === "development"
     ? "http://app.market.local:3000/login"
     : "https://app.market.dev/login";
 
+// @TODO: All these should be config options, not hardcoded.
 const discordURL = "https://discord.gg/ZdSpS4BuGd";
 const blogURL = "https://blog.market.dev";
 const twitterUrl = "https://x.com/marketdotdev";
