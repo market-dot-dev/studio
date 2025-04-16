45c06550edc0fe7d7141b94dab40ce5ee0588447
Thomas Gram <thomas@codeshark.net>
2025-04-16 10:29:55 +0200

    Uptimize themes-panel slightly.

--- components/site/page-editor-sidebar.tsx
+++ components/site/page-editor-sidebar.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useState } from "react";
 import ComponentsPanel from "./components-panel";
-import ThemesPanel from "./themes-panel";
+import { ThemesPanel } from "./themes-panel";
 
 export default function PageEditorSidebar({
   editorRef,
--- components/site/themes-panel.tsx
+++ components/site/themes-panel.tsx
@@ -2,13 +2,9 @@ import siteTemplates from "@/lib/constants/site-templates";
 import { useCallback } from "react";
 import PreviewSection from "./preview-section";
 
-export default function ThemesPanel({
-  insertAtCursor
-}: {
-  insertAtCursor: (prop: string) => void;
-}) {
+export function ThemesPanel({ insertAtCursor }: { insertAtCursor: (prop: string) => void }) {
   const insertSection = useCallback(
-    (section: any) => {
+    (section: { name: string; template: string }) => {
       insertAtCursor(`
 <!--Start ${section.name}-->
 ${section.template}
@@ -25,17 +21,23 @@ ${section.template}
           key={index}
           className="cursor-pointer rounded-md border p-0 text-center align-middle text-sm shadow-sm"
         >
-          <div
+          <button
             className="group relative size-full overflow-hidden rounded-md"
             onClick={() => {
               insertSection(section);
             }}
           >
-            <PreviewSection content={section.template} />
-            <div className="absolute bottom-0 left-0 w-full translate-y-full rounded-b-md bg-black bg-opacity-50 p-2 font-bold text-white opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100">
+            <PreviewSection
+              content={section.template}
+              width={250}
+              height={200}
+              screenWidth={1600}
+              screenHeight={1280}
+            />
+            <div className="bg-opacity/50 absolute bottom-0 left-0 w-full translate-y-full rounded-b-md bg-black p-2 font-bold text-white opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100">
               {section.name}
             </div>
-          </div>
+          </button>
         </div>
       ))}
     </div>
