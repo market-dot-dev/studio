f197beebf97b428c2608b40424d329f3e1ccfaf3
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2025-04-29 15:11:47 -0400

    force success url to /c/ namespace

--- app/app/(payments)/success/page.tsx
+++ app/app/(payments)/success/page.tsx
@@ -4,7 +4,7 @@ import { getRootUrl } from "@/lib/domain";
 import { redirect } from "next/navigation";
 
 const SubscriptionSuccess = () => {
-  redirect(getRootUrl("app", "/"));
+  redirect(getRootUrl("app", "/c/"));
 };
 
 export default SubscriptionSuccess;
