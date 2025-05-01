14d14764dcc487dfad8b0c65a069f2c32580387d
Thomas Gram <thomas@codeshark.net>
2025-05-01 16:35:29 +0200

    Dont run middleware on stripe webhook.

--- middleware.ts
+++ middleware.ts
@@ -17,10 +17,11 @@ export const config = {
      * Match all request paths except for the ones starting with:
      * - _next/static (static files)
      * - _next/image (image optimization files)
-     * - public contract pages (c/contracts/)
+     * - public contract pages (c/contracts/*)
+     * - external webhooks (api/webhook/*)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      */
-    "/((?!_next/static|_next/image|c/contracts/|[\\w-]+\\.\\w+).*)"
+    "/((?!_next/static|_next/image|c/contracts/|api/webhook/|[\\w-]+\\.\\w+).*)"
   ]
 };
 
