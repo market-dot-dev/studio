86fba1bb4803cdb73f73478bd04349f4abf30fc3
Max <73665502+maxgeor@users.noreply.github.com>
2025-05-01 14:03:10 -0400

    fixed OTP input styling

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -1,5 +1,5 @@
 import { Loader } from "lucide-react";
 
 export default function Spinner() {
-  return <Loader className="loading-spinner animate-spin-slow size-3" />;
+  return <Loader aria-label="Loading" className="loading-spinner animate-spin-slow size-3" />;
 }
