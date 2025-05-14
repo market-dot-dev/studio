18e6dec9925026ff11a4841a62415aeca581fc2e
Max <73665502+maxgeor@users.noreply.github.com>
2025-05-14 19:07:13 -0400

    updated checkout subscription status card

--- app/app/(payments)/checkout/[id]/subscription-status.tsx
+++ app/app/(payments)/checkout/[id]/subscription-status.tsx
@@ -1,57 +1,64 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { getRootUrl } from "@/lib/domain";
-import { cn } from "@/lib/utils";
+import { cn, formatDate, getSubscriptionExpiryDateText } from "@/lib/utils";
 import { CircleCheck, Clock } from "lucide-react";
 import Link from "next/link";
 
 interface Props {
   subscriptionId: string;
   tierName: string;
-  isActive: boolean;
   expiryDate?: Date | null;
 }
 
-export function SubscriptionStatus({ subscriptionId, tierName, isActive, expiryDate }: Props) {
+export function SubscriptionStatusCard({ subscriptionId, tierName, expiryDate }: Props) {
   // const subUrl = getRootUrl("app", `/c/subscriptions/${subscriptionId}`); // @NOTE: This page doesn't exist yet
   const subUrl = getRootUrl("app", "/c/");
+  const isExpiring = !!expiryDate;
+
+  const daysRemainingText = isExpiring ? getSubscriptionExpiryDateText(expiryDate) : "";
+
+  const eyebrowText = isExpiring ? daysRemainingText : "Subscribed";
+  const eyebrowColor = isExpiring ? "text-warning" : "text-success";
+  const eyebrowIconFill = isExpiring ? "fill-warning" : "fill-success";
+  const EyebrowIconComponent = isExpiring ? Clock : CircleCheck;
 
   return (
-    <Card className="mx-auto flex w-full flex-col p-5 pt-4 md:max-w-xl lg:max-w-md xl:max-w-lg">
-      <div className="-ml-0.5 mb-3 flex items-center gap-2">
-        {isActive ? (
-          <CircleCheck size={18} className="shrink-0 fill-success stroke-white" />
-        ) : (
-          <Clock size={18} className="shrink-0 fill-warning stroke-white" />
-        )}
-        <p className={cn("font-bold text-sm", isActive ? "text-success" : "text-warning")}>
-          {isActive ? "Subscribed" : "Cancelled"}
-        </p>
+    <Card className="mx-auto flex w-full flex-col md:max-w-xl lg:max-w-md xl:max-w-lg">
+      <div className="-ml-0.5 mb-4 flex items-center gap-2 px-5 pt-4">
+        <EyebrowIconComponent size={18} className={cn("shrink-0 stroke-white", eyebrowIconFill)} />
+        <p className={cn("font-bold text-sm tracking-tightish", eyebrowColor)}>{eyebrowText}</p>
       </div>
-      <div className="mb-5 flex flex-col gap-1">
-        <h3 className="text-lg font-bold text-stone-800">
-          {isActive ? "You're Already Subscribed" : "Your Subscription is Ending Soon"}
+      <div className="mb-6 flex flex-col gap-1 px-5">
+        <h3 className="text-lg font-bold text-foreground">
+          {isExpiring ? `You've Cancelled Your Subscription` : "You're Already Subscribed"}
         </h3>
         <p className="text-sm text-stone-500">
-          {isActive ? (
+          {isExpiring ? (
             <>
-              You currently have an active subscription to{" "}
-              <span className="font-semibold text-stone-800">{tierName}</span>.
+              Your subscription to <span className="font-semibold text-foreground">{tierName}</span>{" "}
+              will be active until{" "}
+              <span className="font-semibold text-foreground">
+                {formatDate(expiryDate?.toLocaleDateString())}
+              </span>
+              .
             </>
           ) : (
             <>
-              Your subscription to <span className="font-semibold text-stone-800">{tierName}</span>{" "}
-              has been cancelled. It'll be active until{" "}
-              <span className="font-semibold text-stone-800">
-                {expiryDate?.toLocaleDateString()}
-              </span>
-              .
+              You currently have an active subscription to{" "}
+              <span className="font-semibold text-foreground">{tierName}</span>.
             </>
           )}
         </p>
       </div>
+      <Separator />
       <Link href={subUrl} passHref>
-        <Button variant="outline" className="w-full">
+        <Button
+          size="lg"
+          variant="outline"
+          className="w-full rounded-b-md rounded-t-none shadow-none"
+        >
           Manage Subscription
         </Button>
       </Link>
