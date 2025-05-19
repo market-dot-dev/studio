0efee218ed812fc1f8ab8fcb270a4503922874bc
Thomas Gram <thomas@codeshark.net>
2025-05-19 21:03:36 +0200

    Add currentOrgId to Session types.

--- app/models/Session.tsx
+++ app/models/Session.tsx
@@ -3,12 +3,14 @@ import { User } from "@/app/generated/prisma";
 export type SessionUser = {
   id: string;
   name?: string;
-  company?: string;
   username?: string;
   gh_id?: number;
   email?: string;
   image?: string;
   onboarding?: string;
+  currentOrgId?: string;
+  // @TODO: Pending migration to Organization
+  company?: string;
   roleId: string;
   stripeCustomerIds: Record<string, string>;
   stripePaymentMethodIds: Record<string, string>;
@@ -25,13 +27,16 @@ export const createSessionUser = (user: User): SessionUser => {
   return {
     id: user.id,
     name: user.name || undefined,
-    company: user.company || undefined,
     username: user.username || user.gh_username || undefined,
     gh_id: user.gh_id || undefined,
     email: user.email || undefined,
-    roleId: user.roleId || "anonymous",
     onboarding: user.onboarding || undefined,
     image: user.image || undefined,
+    currentOrgId: user.currentOrganizationId || undefined,
+
+    // @TODO: Pending migration to Organization
+    company: user.company || undefined,
+    roleId: user.roleId || "anonymous",
     stripeCustomerIds: (user.stripeCustomerIds || {}) as Record<string, string>,
     stripePaymentMethodIds: (user.stripePaymentMethodIds || {}) as Record<string, string>,
     stripeAccountId: user.stripeAccountId || undefined,
