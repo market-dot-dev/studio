82b8acba1b88133abc05682c0655dfae13eb7fe3
Max <73665502+maxgeor@users.noreply.github.com>
2025-05-29 11:26:28 -0400

    user dropdown update

--- components/header/user-dropdown.tsx
+++ components/header/user-dropdown.tsx
@@ -11,8 +11,10 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
+import { LogOut, UserRound } from "lucide-react";
 import { signOut } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function UserDropdown({ user }: { user: SessionUser }) {
   return (
@@ -35,8 +37,8 @@ export default function UserDropdown({ user }: { user: SessionUser }) {
         </Button>
       </DropdownMenuTrigger>
 
-      <DropdownMenuContent className="z-40 w-[250px]" sideOffset={5} align="end" alignOffset={0}>
-        <div className="flex items-center gap-3 p-2">
+      <DropdownMenuContent className="z-40 w-[225px]" sideOffset={5} align="end" alignOffset={0}>
+        <div className="flex items-center gap-3 py-1 pl-[5px] pr-3">
           <Image
             src={user.image ?? `https://avatar.vercel.sh/${user.id}`}
             width={28}
@@ -51,6 +53,12 @@ export default function UserDropdown({ user }: { user: SessionUser }) {
         <DropdownMenuSeparator />
 
         <DropdownMenuGroup>
+          <DropdownMenuItem asChild>
+            <Link href="/settings" className="flex items-center gap-2">
+              <UserRound className="!h-4.5 !w-4.5" />
+              Profile
+            </Link>
+          </DropdownMenuItem>
           <DropdownMenuItem
             onClick={() =>
               signOut({
@@ -59,6 +67,7 @@ export default function UserDropdown({ user }: { user: SessionUser }) {
               })
             }
           >
+            <LogOut className="!h-4.5 !w-4.5" />
             Logout
           </DropdownMenuItem>
         </DropdownMenuGroup>
