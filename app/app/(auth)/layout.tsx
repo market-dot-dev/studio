e0b3890043ff8d1a06bcbc00ca44484b11487bb6
Max Georgopoulos <max123358@gmail.com>
2025-06-16 11:21:10 -0400

    update login background color

--- app/app/(auth)/layout.tsx
+++ app/app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex size-full min-h-screen flex-col items-center justify-center bg-stone-100 px-6 py-12 sm:px-12">
+    <div className="flex size-full min-h-screen flex-col items-center justify-center bg-stone-150 px-6 py-12 sm:px-12">
       <div className="flex size-full max-w-screen-xl flex-col items-center justify-between gap-x-12 gap-y-24 lg:flex-row lg:justify-center">
         <div className="flex w-full max-w-md flex-col justify-center pb-[72px]">
           <div className="mx-auto w-full max-w-sm">{children}</div>
