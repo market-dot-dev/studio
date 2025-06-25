d442123e0fbafd38e6571b91482f3cb0abb575cc
Max Georgopoulos <max123358@gmail.com>
2025-06-25 14:30:27 -0400

    undo spinner component update

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -1,11 +1,5 @@
-import { cn } from "@/lib/utils";
 import { Loader } from "lucide-react";
 
-export function Spinner({ className }: { className?: string }) {
-  return (
-    <Loader
-      aria-label="Loading"
-      className={cn("loading-spinner animate-spin-slow size-3", className)}
-    />
-  );
+export function Spinner() {
+  return <Loader aria-label="Loading" className="loading-spinner animate-spin-slow size-3" />;
 }
