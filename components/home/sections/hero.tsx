da7341a43713d257c13304cc4345fd48923d594b
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2025-08-22 17:14:45 -0400

    Update hero.tsx

--- components/home/sections/hero.tsx
+++ components/home/sections/hero.tsx
@@ -7,13 +7,12 @@ export default function Hero() {
     <>
       <div className="mx-auto flex w-full max-w-[800px] flex-col items-center gap-12 px-6 pt-6 md:pt-10 lg:pt-11 tracking-tight md:gap-y-16 lg:max-w-[var(--marketing-max-width)] lg:px-16 ">
         <div className="flex size-full flex-col items-center">
-          <GradientHeading className="mb-3 whitespace-nowrap text-center text-[clamp(24px,8vw,37px)] font-bold !leading-[0.9] tracking-[-0.0525em] text-marketing-primary xs:text-marketing-xl sm:mb-4 sm:text-marketing-2xl md:text-marketing-3xl md:tracking-[-0.045em] lg:mb-5 lg:text-marketing-4xl xl:text-marketing-5xl">
-            The easiest way to sell
-            <br /> developer se<span className="tracking-normal">r</span>vices.
+          <GradientHeading className="mb-3 whitespace-nowrap text-center text-[clamp(24px,8vw,37px)] font-bold !leading-[0.9] tracking-[-0.0525em] text-marketing-primary xs:text-marketing-lg sm:mb-4 sm:text-marketing-xl md:text-marketing-2xl md:tracking-[-0.045em] lg:mb-5 lg:text-marketing-2xl xl:text-marketing-3xl">
+            Sales and marketing tools <br />for developer experts.
           </GradientHeading>
           <p className="mb-4 max-w-[44ch] text-pretty text-center text-marketing-sm !leading-tight xs:mb-5 xs:text-balance sm:mb-6 sm:text-[clamp(16px,12vw,20px)] lg:text-marketing-md">
-            The all-in-one toolkit for to offer premium services & manage your{" "}
-            <br className="xs:hidden" /> business. Get started in minutes.
+            The AI-native marketing, CRM and payment toolkit for freelancers.{" "}
+            <br className="xs:hidden" />{" "}Let's grow your business, today.
           </p>
           <div>
             <ClaimStoreForm id="hero" />
