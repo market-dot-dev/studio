0723b367595cce71e661b39616847c48523e6f99
Tarun Sachdeva <tarun.sachdeva@gmail.com>
2025-10-29 14:46:33 -0400

    update stripe api version

--- app/services/stripe/create-stripe-client.ts
+++ app/services/stripe/create-stripe-client.ts
@@ -13,7 +13,7 @@ export async function createStripeClient(stripeAccountId?: string): Promise<Stri
   }
 
   const options: Stripe.StripeConfig = {
-    apiVersion: "2025-06-30.basil"
+    apiVersion: "2025-07-30.basil"
   };
 
   if (stripeAccountId) {
